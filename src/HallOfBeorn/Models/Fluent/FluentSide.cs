8d77a8454b478ff114aae9d62e9244d492b8db50
Dan Poage <dan.poage@gmail.com>
2015-05-16 22:44:21 -0500

    1. Removed HallOfBeorn.Models.csproj project from git 2. Started work on FluentCard and FluentSide classes to support fluently-building cards

---
+++ src/HallOfBeorn/Models/Fluent/FluentSide.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HallOfBeorn.Models.Fluent
+{
+    public class FluentSide
+    {
+        public FluentSide()
+        {
+            Artists = new List<Artist>();
+        }
+
+        public List<Artist> Artists { get; private set; }
+    }
+}
---
+++ src/HallOfBeorn/Models/Fluent/StatType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HallOfBeorn.Models.Fluent
+{
+    public enum StatType: byte
+    {
+        None = 0,
+        Unique,
+        Threat_Cost,
+        Willpower,
+        Attack,
+        Defense,
+        Hit_Points,
+        Engagement_Cost,
+        Threat,
+        Resource_Cost,
+        Quest_Points
+    }
+}
