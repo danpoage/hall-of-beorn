cf629694813be78d4764d04ad0b3141b2debbab6
Dan Poage <dan.poage@gmail.com>
2015-05-20 19:19:53 -0500

    Added RulesUrl links to TheNinInEilphProduct, CelebrimborsSecretProduct and TheAntleredCrownProduct

--- src/HallOfBeorn/Models/Products/TheAntleredCrownProduct.cs
+++ src/HallOfBeorn/Models/Products/TheAntleredCrownProduct.cs
@@ -10,6 +10,8 @@ namespace HallOfBeorn.Models.Products
         public TheAntleredCrownProduct()
             : base("The Antlered Crown", "MEC31", ImageType.Jpg)
         {
+            RulesUrl = "https://images-cdn.fantasyflightgames.com/filer_public/a9/19/a91988d8-41a3-4653-9b3e-c2a970c1b2a8/mec31-the-antlered-crown-rulesheet.pdf";
+
             CardSets.Add(CardSet.TheAntleredCrown);
         }
     }
--- src/HallOfBeorn/Models/Products/TheNinInEilphProduct.cs
+++ src/HallOfBeorn/Models/Products/TheNinInEilphProduct.cs
@@ -10,6 +10,8 @@ namespace HallOfBeorn.Models.Products
         public TheNinInEilphProduct()
             : base("The Nîn-in-Eilph", "MEC29", ImageType.Png)
         {
+            RulesUrl = "https://images-cdn.fantasyflightgames.com/filer_public/2d/5c/2d5ce50f-0364-4ece-a880-85a9f7d840a6/mec29-the-nin-in-eilph-rulesheet.pdf";
+
             CardSets.Add(CardSet.NinInEilph);
         }
     }
