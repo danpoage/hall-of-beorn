04652f84f573248780a44c143e73db58abc9d7e4
Dan Poage <dan.poage@gmail.com>
2015-08-12 19:15:03 -0500

    1.Updated autocomplete3.js to autocomplete4.js for The Ruins of Belegost, Escape from Mount Gram and The Land of Shadow 2. Added normalized titles for Naurlhug, The Book of Aule's Children and Naurlhug's Lair

--- src/HallOfBeorn/Models/Sets/TheRuinsOfBelegost.cs
+++ src/HallOfBeorn/Models/Sets/TheRuinsOfBelegost.cs
@@ -153,6 +153,7 @@ Page 4/4",
                 .WithText("If unattached, the fist player claims Keys of Belegost and attaches it to a hero he controls.\r\nAction: Exhaust Keys of Belegost to choose a hero. Add 1 resource to that hero's resource pool.")
                 .WithInfo(11, 1, Artist.Lucas_Durham));
             Cards.Add(Card.Objective("Book of Aulë's Children", "", "The Ruins of Belegost")
+                .WithNormalizedTitle("Book of Aule's Children")
                 .WithUnique()
                 .WithTraits("Artifact.", "Item.")
                 .WithKeywords("Loot.")
@@ -171,6 +172,7 @@ Page 4/4",
                 .WithText("If unattached, the fist player claims Blue Mountain Gem and attaches it to a hero he controls.\r\nAction: Exhaust Blue Mountain Gem to choose a player. Reduce that player's threat by 1.")
                 .WithInfo(14, 1, Artist.Lucas_Durham));
             Cards.Add(Card.Enemy("Naurlhûg", "", "The Ruins of Belegost", 20, 255, 7, 5, 20)
+                .WithNormalizedTitle("Naurlhug")
                 .WithUnique()
                 .WithTraits("Dragon.")
                 .WithText("Immune to player card effects, Cannot take damage.\r\nX is the number of resources on Stalking the Ruins.\r\nForced: When Naurlhûg makes an attack, the defending player assigns X damage among characters he controls.")
@@ -203,6 +205,7 @@ Page 4/4",
                 .WithShadow("Shadow: If you control a Loot objective, put Blue Mountain Goblin into play engaged with you and deal it a shadow card.")
                 .WithInfo(20, 3, Artist.Sam_Lamont));
             Cards.Add(Card.Location("Naurlhûg's Lair", "", "The Ruins of Belegost", 4, 6)
+                .WithNormalizedTitle("Naurlhug's Lair")
                 .WithUnique()
                 .WithTraits("Underground.")
                 .WithKeywords("Discover 5.")
