8a1bc8f90da7114ce2926871f28102f3a266ebe6
Dan Poage <dan.poage@gmail.com>
2015-09-23 00:26:24 -0500

    Finished fixing encounter card text for TheWatcherintheWater

--- src/HallOfBeorn/Models/Sets/TheWatcherInTheWater.cs
+++ src/HallOfBeorn/Models/Sets/TheWatcherInTheWater.cs
@@ -64,6 +64,7 @@ namespace HallOfBeorn.Models.Sets
                 EncounterSet = "The Watcher in the Water",
                 Quantity = 3,
                 Keywords = new List<string>() { "Doomed 5." },
+                HtmlTemplate = "<p>{keyword:Doomed+5.@Doomed 5.}</p><p class='flavor-text'>...there came a soft sound: a swish, followed by a plop, as if a fish had disturbed the still surface of the water. Turning quickly they saw ripples, black-edged with shadow in the waning light: great rings were widening outwards from a point far out in the lake. There was a bubbling noise, and then silence<br>&ndash;The Fellowship of the Ring</p>",
                 Number = 71,
                 Artist = Artist.Cristi_Balanescu
             });
@@ -77,6 +78,7 @@ namespace HallOfBeorn.Models.Sets
                 Quantity = 1,
                 IsUnique = true,
                 Text = "Progress tokens that would be placed on Doors of Durin are instead placed on the current quest card.\r\nAction: Each player may discard any number of cards from his hand. Then, discard the top card of the encounter deck. If the first letter of the encounter card's title matches that of one of the discarded player cards, add Doors of Durin to your victory display. (Limit once per round.)",
+                HtmlTemplate = "<p>Progress tokens that would be placed on {self} are instead placed on the current quest card.</p><p><b>Action:</b> Each player may discard any number of cards from his hand. Then, discard the top card of the encounter deck. If the first letter of the encounter card's title matches that of one of the discarded player cards, add {self} to your victory display. (Limit once per round.)</p>{victory:3}",
                 Threat = 2,
                 QuestPoints = 255,
                 VictoryPoints = 3,
@@ -110,6 +112,7 @@ namespace HallOfBeorn.Models.Sets
                 Defense = 0,
                 HitPoints = 3,
                 Text = "Forced: When Grasping Tentacle is attacked, discard the top card of the encounter deck. If that card has a shadow effect or is a Tentacle enemy, attach this card to an attacking character as a Tentacle attachment with the text: 'Attached character's Attack and Defense are reduced to 0.'",
+                HtmlTemplate = "<p><b>Forced:</b> When {self} is attacked, discard the top card of the encounter deck. If that card has a shadow effect or is a {trait:Tentacle.@Tentacle} enemy, attach this card to an attacking character as a {trait:Tentacle.@Tentacle} attachment with the text: &quot;Attached character's {Attack} and {Defense} are reduced to 0.&quot;</p>",
                 Threat = 2,
                 Number = 73,
                 Artist = Artist.Mark_Tarrisse
@@ -165,6 +168,7 @@ namespace HallOfBeorn.Models.Sets
                 Quantity = 2,
                 Text = "Forced: After you travel to Makeshift Passage, place 2 progress tokens on the current quest card, bypassing any active location.",
                 FlavorText = "They reached the strip of dry land between the lake and the cliffs: it was narrow, often hardly a dozen yards across, and encumbered with fallen rock and stones; but they found a way, hugging the cliff, and keeping as far from the dark water as they might. - The Fellowship of the Ring",
+                HtmlTemplate = "<p><b>Forced:</b> After you travel to {self}, place 2 progress tokens on the current quest card, bypassing any active location.</p><p class='flavor-text'>They reached the strip of dry land between the lake and the cliffs: it was narrow, often hardly a dozen yards across, and encumbered with fallen rock and stones; but they found a way, hugging the cliff, and keeping as far from the dark water as they might.<br>&ndash;The Fellowship of the Ring</p>",
                 Threat = 1,
                 QuestPoints = 5,
                 Number = 68,
@@ -180,6 +184,7 @@ namespace HallOfBeorn.Models.Sets
                 Quantity = 2,
                 Text = "No more than 1 progress token can be placed on Perilous Swamp each round.",
                 Shadow = "Shadow: Remove 1 progress token from the current quest.",
+                HtmlTemplate = "<p>No more than 1 progress token can be placed on {self} each round</p><p class='flavor-text'><b>Shadow:</b> Remove 1 progress token from the current quest.</p>",
                 Threat = 4,
                 QuestPoints = 2,
                 Number = 67,
@@ -235,6 +240,7 @@ namespace HallOfBeorn.Models.Sets
                 Quantity = 3,
                 EasyModeQuantity = 1,
                 Text = "When Revealed: Discard the top card of the encounter deck. If the discarded card is a Tentacle enemy, add that card to the staging area and raise each player's threat by 5.",
+                HtmlTemplate = "<p><b>When Revealed:</b> Discard the top card of the encounter deck. If the discarded card is a {trait:Tentacle.@Tentacle} enemy, add that card to the staging area and raise each player's threat by 5.</p><p class='flavor-text'>When they came to the northernmost corner of the lake they found a narrow creek that barreed their way. It was green and stagnant, thrust out like a slimy arm towards the enclosing hills.<br>&ndash;The Fellowship of the Ring</p>",
                 Threat = 3,
                 QuestPoints = 3,
                 Number = 69,
@@ -270,6 +276,7 @@ namespace HallOfBeorn.Models.Sets
                 Defense = 1,
                 HitPoints = 3,
                 Text = "Forced: When Striking Tentacle attacks, discard the top card of the encounter deck. If that card has a shadow effect or is a Tentacle enemy, this attack is considered undefended.",
+                HtmlTemplate = "<p><b>Forced:</b> When {self} attacks, discard the top card of the encounter deck. If that card has a shadow effect or is a {trait:Tentacle.@Tentacle} enemy, this attack is considered undefended.</p>",
                 Threat = 2,
                 Number = 75,
                 Artist = Artist.Cristi_Balanescu
@@ -298,10 +305,9 @@ namespace HallOfBeorn.Models.Sets
                 EncounterSet = "The Watcher in the Water",
                 Quantity = 1,
                 FlavorText = "The others swung round and saw the waters of the lake seething, as if a host of snakes were swimming up from the southern end. - The Fellowship of the Ring\r\nThe Doors of Durin are blocked by an ancient spell. You must figure out a way into the mines before the Seething bog and its Watcher consumes you all.",
-                OppositeText = 
-@"When Revealed: Add The Watcher to the staging area. Doors of Durin becomes the active location, moving any previous active location to the staging area. Shuffle all Tentacle cards in the encounter discard pile back into the encounter deck.
-
-If the players have at least 3 victory points and defeat this stage, they have won the game.",
+                OppositeText = "When Revealed: Add The Watcher to the staging area. Doors of Durin becomes the active location, moving any previous active location to the staging area. Shuffle all Tentacle cards in the encounter discard pile back into the encounter deck.\r\nIf the players have at least 3 victory points and defeat this stage, they have won the game.",
+                HtmlTemplate = "<p class='flavor-text'>The others swung round and saw the waters of the lake seething, as if a host of snakes were swimming up from the southern end.<br>&ndash;The Fellowship of the Ring</p><p class='flavor-text'>The Doors of Durin are blocked by an ancient spell. You must figure out a way into the mines before the Seething bog and its Watcher consumes you all.</p>",
+                HtmlTemplate2 = "<p><b>When Revealed:</b> Add <a title='The Watcher (The Watcher in the Water)' href='/Cards/Details/The-Watcher-TWitW' target='_blank'>The Watcher</a> to the staging area. <a title='Doors of Durin (The Watcher in the Water)' href='/Cards/Details/Doors-of-Durin-TWitW' target='_blank'>Doors of Durin</a> becomes the active location, moving any previous active location to the staging area. Shuffle all {trait:Tentacle.@Tentacle} cards in the encounter discard pile back into the encounter deck.</p><p><b>If the players have at least 3 victory points and defeat this stage, they have won the game.</b></p>",
                 QuestPoints = 5,
                 Number = 64,
                 Artist = Artist.Adam_Schumpert,
@@ -322,6 +328,7 @@ If the players have at least 3 victory points and defeat this stage, they have w
                 HitPoints = 9,
                 Text = "While there is another Tentacle enemy in play, The Watcher cannot be optionally engaged.\r\nIf The Watcher is in the staging area at the end of the combat phase, each player must deal 3 damage to 1 character he controls.",
                 Keywords = new List<string>() { "Regenerate 2." },
+                HtmlTemplate = "<p>{keyword:Regenerate+2.@Regenerate 2}. While there is another {trait:Tentacle.@Tentacle} enemy in play, {self} cannot be optionally engaged.</p><p>If {self} is in the staging area at the end of the combat phase, each player must deal 3 damage to 1 character he controls.</p>{victory:3}",
                 Threat = 4,
                 VictoryPoints = 3,
                 Number = 72,
@@ -341,6 +348,7 @@ If the players have at least 3 victory points and defeat this stage, they have w
                 Defense = 0,
                 HitPoints = 3,
                 Text = "Forced: When Thrashing Tentacle is attacked, discard the top card of the encounter deck. If that card has a shadow effect or is a Tentacle enemy, deal the damage from the attack to 1 character an attacking player controls (ignoring defense).",
+                HtmlTemplate = "<p><b>Forced:</b> When {self} is attacked, discard the top card of the encounter deck. If that card has a shadow effect or is a {trait:Tentacle.@Tentacle} enemy, deal the damage from the attack to 1 character an attacking player controls <i>(ignoring defense)</i>.</p>",
                 Threat = 2,
                 Number = 74,
                 Artist = Artist.Cristi_Balanescu
@@ -359,6 +367,8 @@ If the players have at least 3 victory points and defeat this stage, they have w
                 FlavorText = "Elrond has asked you to scout the Mines of Moria on your return to Lorien, hoping to discover if it is the source of increased Orc activity along the Misty Mountains.",
                 OppositeText = "When Revealed: Reveal cards from the encounter deck and add them to the staging area until there is at least X threat in the staging area. X is twice the number of players in the game.",
                 OppositeFlavorText = "Your approach is blocked by a dark lake that slumbers beneath the face of the cliff. You must search for a way around the water.",
+                HtmlTemplate = "<p class='flavor-text'>Elrond has asked you to scout the Mines of Moria on your return to Lorien, hoping to discover if it is the source of increased Orc activity along the Misty Mountains.</p><p><b>Setup:</b> Remove <a title='The Watcher (The Watcher in the Water)' href='/Cards/Details/The-Watcher-TWitW' target='_blank'>The Watcher</a> and <a title='Doors of Durin (The Watcher in the Water)' href='/Cards/Details/Doors-of-Durin-TWitW' target='_blank'>Doors of Durin</a> from the encounter deck and set them aside, out of play.</p>",
+                HtmlTemplate2 = "<p class='flavor-text'>Your approach is blocked by a dark lake that slumbers beneath the face of the cliff. You must search for a way around the water.</p><p><b>When Revealed:</b> Reveal cards from the encounter deck and add them to the staging area until there is at least X {Threat} in the staging area. X is twice the number of players in the game.</p>",
                 Number = 63,
                 Artist = Artist.Jason_Juta,
                 IncludedEncounterSets = new List<EncounterSet> { EncounterSet.MistyMountains }
@@ -393,6 +403,7 @@ If the players have at least 3 victory points and defeat this stage, they have w
                 Quantity = 4,
                 EasyModeQuantity = 2,
                 Text = "When Revealed: The first player attaches Wrapped! to a hero he controls. (Counts as a Tentacle attachment with the text: 'Limit 1 per hero. Attached hero cannot exhaust or ready. At the end of the round, discard attached hero from play. Combat Action: Exhaust a hero you control without a Tentacle attachment to discard Wrapped!.')",
+                HtmlTemplate = "<p><b>When Revealed:</b> The first player attaches {self} to a hero he controls. Counts as a {trait:Tentacle.@Tentacle} attachment with the text: <blockquote>Limit 1 per hero. Attached hero cannot exhaust or ready. At the end of the round, discard attached hero from play.<br><b>Combat Action:</b> Exhaust a hero you control without a {trait:Tentacle.@Tentacle} attachment to discard {self}.</blockquote></p>",
                 Number = 76,
                 Artist = Artist.Cristi_Balanescu
             });
