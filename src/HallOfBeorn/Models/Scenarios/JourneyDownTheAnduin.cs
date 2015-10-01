8385750cc6898933ec3d08068f6699a75a44ed60
Dan Poage <dan.poage@gmail.com>
2015-10-01 00:22:53 -0500

    Started work on JourneyDownTheAnduin scenario.

---
+++ src/HallOfBeorn/Models/Scenarios/JourneyDownTheAnduin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HallOfBeorn.Models.Scenarios
+{
+    public class JourneyDownTheAnduin : CoreSetScenario
+    {
+        public JourneyDownTheAnduin()
+        {
+            Number = 2;
+            Title = "Journey Down the Anduin";
+            ProductName = "Core Set";
+        }
+    }
+}
