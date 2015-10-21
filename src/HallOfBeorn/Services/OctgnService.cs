6655b28db5638422c084f7b6c7493f6ccbd477e4
Dan Poage <dan.poage@gmail.com>
2015-10-21 00:38:36 -0500

    Created OctgnService with GetOctgnId and GetSlug methods for mapping OCTGN IDs to Hall of Beorn card slugs

---
+++ src/HallOfBeorn/Services/OctgnService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HallOfBeorn.Services
+{
+    public class OctgnService
+    {
+        public Guid GetOctgnId(string slug)
+        {
+            return Guid.Empty;
+        }
+
+        public string GetSlug(Guid octgnId)
+        {
+            return string.Empty;
+        }
+    }
+}
