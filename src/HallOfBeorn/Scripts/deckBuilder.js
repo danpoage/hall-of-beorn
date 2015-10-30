51e97fd8732ff4073f4e1244d0e0ff7db3831bf6
Dan Poage <dan.poage@gmail.com>
2015-10-30 16:23:43 -0500

    Created saveOctgnDeck function and started work to support saving a deck to an .o8d OCTGN deck file.

--- src/HallOfBeorn/Scripts/deckBuilder.js
+++ src/HallOfBeorn/Scripts/deckBuilder.js
@@ -7,12 +7,11 @@
         var imageHtml = '<img class="deck-card-image deck-card-image-hidden" src="' + card.CardImage + '"/>';
         var sphereHtml = '<img class="deck-sphere-icon" src="' + card.SphereIcon + '"/>';
         var titleHtml = '<a class="deck-card-title" href="' + card.Url + '" target="_blank">' + card.Title + '</a>';
-        var removeHtml = '<img class="deck-item-remove" src="/Images/close-button.png"/>'
+        var removeHtml = '<img class="deck-item-remove" src="/Images/close-button.png"/>';
 
-        var countHtml = card.MaxPerDeck == 3 ? '<select class="deck-item-count"><option>1</option><option>2</option><option>3</option></select>' : '<select  class="deck-item-count-one" disabled><option>1</option></select>';
+        var countHtml = card.MaxPerDeck == 3 ? '<select class="deck-item-count"><option value="1">1</option><option value="2">2</option><option value="3">3</option></select>' : '<select  class="deck-item-count-one" disabled><option value="1">1</option></select>';
 
         var octgnGuid = card.OctgnGuid;
-        console.log('octgn guid: ' + octgnGuid);
         var selector = false;
         var itemClass = 'deck-card-item';
         var titleClass = '';
@@ -21,7 +20,7 @@
             case 'Hero':
                 selector = '#heroList';
                 itemClass = 'hero-card-item';
-                itemHtml = '<div><img class="hero-card-image" src="' + card.CardImage + '"/></div><div>' + sphereHtml + '<a href="' + card.Url + '" target="_blank">' + card.Title + '</a>' + removeHtml + '</div>';
+                itemHtml = '<div><img class="hero-card-image" src="' + card.CardImage + '"/></div><div>' + sphereHtml + titleHtml + removeHtml + '</div>';
                 break;
             case 'Ally':
                 selector = '#allyList';
@@ -42,6 +41,78 @@
         if (selector) {
             $(selector).append('<li data-octgn="' + octgnGuid + '" class="deck-item ' + itemClass + '">' + itemHtml + '</li>');
         }
+    }
+
+    $('#octgnExport').click(function (e) {
+        console.log('octgnExport');
+        saveOctgnDeck();
+    });
+
+    function saveOctgnDeck() {
+        var xml = '<?xml version="1.0" encoding="utf-8" standalone="yes"?><deck game="a21af4e8-be4b-4cda-a6b6-534f9717391f">';
+
+        xml += '<section name="Hero" shared="False">';
+        $('#heroList').find('.deck-item').each(function (index, item) {
+            var title = $(item).find('.deck-card-title').html(), 
+                octgnGuid = $(item).data('octgn');
+            
+            xml += '<card qty="1" id="' + octgnGuid + '">' + title + '</card>';
+        });
+        xml += '</section>';
+
+        xml += '<section name="Ally" shared="False">';
+        $('#allyList').find('.deck-item').each(function (index, item) {
+            var title = $(item).find('.deck-card-title').html(),
+                count = $(item).find('.deck-item-count').val(),
+                octgnGuid = $(item).data('octgn');
+
+            console.log('count: ' + count);
+
+            xml += '<card qty="' + count +'" id="' + octgnGuid + '">' + title + '</card>';
+        });
+        xml += '</section>';
+
+        xml += '<section name="Attachment" shared="False">';
+        $('#attachmentList').find('.deck-item').each(function (index, item) {
+            var title = $(item).find('.deck-card-title').html(),
+                count = $(item).find('.deck-item-count').val(),
+                octgnGuid = $(item).data('octgn');
+
+            console.log('count: ' + count);
+
+            xml += '<card qty="' + count + '" id="' + octgnGuid + '">' + title + '</card>';
+        });
+        xml += '</section>';
+
+        xml += '<section name="Event" shared="False">';
+        $('#eventList').find('.deck-item').each(function (index, item) {
+            var title = $(item).find('.deck-card-title').html(),
+                count = $(item).find('.deck-item-count').val(),
+                octgnGuid = $(item).data('octgn');
+
+            console.log('count: ' + count);
+
+            xml += '<card qty="' + count + '" id="' + octgnGuid + '">' + title + '</card>';
+        });
+        xml += '</section>';
+
+        xml += '<section name="Side Quest" shared="False">';
+        $('#sideQuestList').find('.deck-item').each(function (index, item) {
+            var title = $(item).find('.deck-card-title').html(),
+                count = $(item).find('.deck-item-count').val(),
+                octgnGuid = $(item).data('octgn');
+
+            console.log('count: ' + count);
+
+            xml += '<card qty="' + count + '" id="' + octgnGuid + '">' + title + '</card>';
+        });
+        xml += '</section>';
+
+        xml += '</deck>';
+
+        //var aFileParts = [ xml ];
+        //var blob = new Blob(aFileParts, { type: 'application/octgn' });
+        //window.open(URL.createObjectURL(blob));
     }
 
     $('#deckContainer').on('mouseenter', '.deck-card-title', function (e) {
