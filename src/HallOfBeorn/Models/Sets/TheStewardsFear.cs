9b411c3ee09f31a4a2c8b0e51a3fc6c5a9a4ed0b
Dan Poage <dan.poage@gmail.com>
2016-09-27 20:14:57 -0500

    Change TheStewardsFear to use high-quality JPG images

--- src/HallOfBeorn/Models/Sets/TheStewardsFear.cs
+++ src/HallOfBeorn/Models/Sets/TheStewardsFear.cs
@@ -16,7 +16,7 @@ namespace HallOfBeorn.Models.Sets
             Cycle = "Against the Shadow";
 
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Hirluin the Fair",
                 Id = "85699571-9cc3-4ccc-98b0-0e1664893ef5",
                 CardType = CardType.Hero,
@@ -36,7 +36,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Tony_Foti
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Warrior of Lossarnach",
                 Id = "2e84d805-365c-47ea-9c4f-e3f75daeb9a6",
                 CardType = CardType.Ally,
@@ -54,7 +54,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Mark_Winters
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Gaining Strength",
                 Id = "add42229-f5d1-4c39-a5dd-ec00ad7d0d76",
                 CardType = CardType.Event,
@@ -67,7 +67,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Magali_Villeneuve
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Knights of the Swan",
                 Id = "c00844d6-1c3c-4e8c-a46c-8de15b8408df",
                 CardType = CardType.Ally,
@@ -85,7 +85,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Marcia_George_Bogdan
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Gondorian Shield",
                 Id = "09134509-191b-4903-b4b5-5e650f8143c1",
                 CardType = CardType.Attachment,
@@ -101,7 +101,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Owen_William_Weber
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Ethir Swordsman",
                 Id = "1c149f93-9e3b-42fa-878c-80b29563a283",
                 CardType = CardType.Ally,
@@ -119,7 +119,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Adam_Schumpert
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Ring of Barahir",
                 Id = "0ef9e385-b7e5-4676-a690-2dd8031aa8c8",
                 CardType = CardType.Attachment,
@@ -133,7 +133,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Sara_Biddle
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Anfalas Herdsman",
                 Id = "4cb4741d-c9d8-4d62-ab4f-50fa80c59fbb",
                 CardType = CardType.Ally,
@@ -151,7 +151,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Blake_Henriksen
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Mithrandir's Advice",
                 Id = "3c47d373-62bc-4321-9dc1-9b86b2046f68",
                 CardType = CardType.Event,
@@ -164,7 +164,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Nacho_Molina
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "A Good Harvest",
                 Id = "e02f317e-736c-40a0-8d73-45509fce9ef8",
                 CardType = CardType.Event,
@@ -177,7 +177,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Trudi_Castle
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Underworld Dissident",
                 Id = "eb9f723d-0f74-4d10-89e7-b046313a1e91",
                 CardType = CardType.Enemy,
@@ -195,7 +195,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Dan_Howard
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Daughter of Berúthiel",
                 NormalizedTitle = "Daughter of Beruthiel",
                 Id = "408148e1-3391-4734-aeac-3e3a7b69c1c1",
@@ -214,7 +214,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Anna_Steinbauer
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "The Hand of Castamir",
                 Id = "21f520fd-5efe-41f9-9f02-7a65e36eb31f",
                 CardType = CardType.Enemy,
@@ -232,7 +232,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Tiziano_Baracchi
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Telemnar's Bane",
                 Id = "c460b9c0-0239-4a74-99cb-3c302429db44",
                 CardType = CardType.Enemy,
@@ -250,7 +250,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Aurelien_Hubert
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Sewers",
                 Id = "03587572-2760-48b5-9b08-e01ccdc9788f",
                 CardType = CardType.Location,
@@ -265,7 +265,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Titus_Lunter
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Roots of Mindolluin",
                 Id = "5e39ebfc-287a-49d5-9369-49dbfb0fd665",
                 CardType = CardType.Location,
@@ -282,7 +282,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Rafal_Hrynkiewicz
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Storehouse",
                 Id = "b89a5a01-88c0-434a-a061-1654c6f84477",
                 CardType = CardType.Location,
@@ -299,7 +299,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Titus_Lunter
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Houses of the Dead",
                 Id = "35055e3e-d806-4c21-ad37-8177d70be329",
                 CardType = CardType.Location,
@@ -317,7 +317,7 @@ namespace HallOfBeorn.Models.Sets
                 Artist = Artist.Titus_Lunter
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "The Fourth Star",
                 Id = "b2acb892-ab3b-4037-8751-c036f9f6f8bd",
                 CardType = CardType.Location,
@@ -336,7 +336,7 @@ Response: When The Fourth Star leaves play as an explored location, each player 
                 Artist = Artist.Sabin_Boykinov
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Unwelcome Discovery",
                 Id = "fc4f9f69-7870-4449-b89b-cd7b4dc158d0",
                 CardType = CardType.Treachery,
@@ -348,7 +348,7 @@ Response: When The Fourth Star leaves play as an explored location, each player 
                 Artist = Artist.David_Gaillet
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "A Knife in the Back",
                 Id = "c4218a3e-6a6b-47a0-88c5-cdbcda3fb5eb",
                 CardType = CardType.Treachery,
@@ -360,7 +360,7 @@ Response: When The Fourth Star leaves play as an explored location, each player 
                 Artist = Artist.Carmen_Cianelli
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "False Lead",
                 Id = "fe7448a2-e2e2-4e81-95f4-2ac585160433",
                 CardType = CardType.Treachery,
@@ -372,7 +372,7 @@ Response: When The Fourth Star leaves play as an explored location, each player 
                 Artist = Artist.Darek_Zabrocki
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "A Scrap of History",
                 Id = "a8494d53-c8a4-4f74-b3e1-2e4183aeea2a",
                 CardType = CardType.Objective,
@@ -385,7 +385,7 @@ Response: When The Fourth Star leaves play as an explored location, each player 
                 Artist = Artist.Aurelien_Hubert
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Secret Map",
                 Id = "dce8ff38-bcc7-49b4-ba27-ae3e2cac1d32",
                 CardType = CardType.Objective,
@@ -398,7 +398,7 @@ Response: When The Fourth Star leaves play as an explored location, each player 
                 Artist = Artist.Ilich_Henriquez
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "A Prisoner",
                 Id = "23c33968-01db-4f79-94bd-45ea1e0d50d0",
                 CardType = CardType.Objective,
@@ -411,7 +411,7 @@ Response: When The Fourth Star leaves play as an explored location, each player 
                 Artist = Artist.Even_Mehl_Amundsen
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Unholy Alliance",
                 Id = "2bf5a4d1-9d74-4e1a-8725-cf4a3a47dde9",
                 CardType = CardType.Objective,
@@ -423,7 +423,7 @@ Response: When The Fourth Star leaves play as an explored location, each player 
                 Artist = Artist.Blake_Henriksen
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Up in Flames",
                 Id = "1dcf16e8-8882-4b61-93ac-a7da643c52bd",
                 CardType = CardType.Objective,
@@ -435,7 +435,7 @@ Response: When The Fourth Star leaves play as an explored location, each player 
                 Artist = Artist.Marcia_George_Bogdan
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Poisoned Counsels",
                 Id = "4bcc7bde-4223-411e-a612-9459a1502743",
                 CardType = CardType.Objective,
@@ -448,7 +448,7 @@ Response: When The Fourth Star leaves play as an explored location, each player 
                 Artist = Artist.David_A_Nash
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "Conspiracy",
                 StageNumber = 1,
                 Id = "4e12f20f-aec3-4311-a656-10a517fd97fe",
@@ -469,7 +469,7 @@ If there are 4 or more resource tokens on Conspiracy, advance to the next stage.
                 IncludedEncounterSets = new List<EncounterSet> { EncounterSet.Brigands, EncounterSet.StreetsOfGondor }
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "The Grand Design",
                 StageNumber = 2,
                 Id = "21ee317d-9aca-43be-9782-521539827cb8",
@@ -489,7 +489,7 @@ If there are 4 or more resource tokens on The Grand Design, advance to the next 
                 IncludedEncounterSets = new List<EncounterSet> { EncounterSet.Brigands, EncounterSet.StreetsOfGondor }
             });
             Cards.Add(new Card() {
-                ImageType = Models.ImageType.Png,
+                ImageType = Models.ImageType.Jpg,
                 Title = "The Confrontation",
                 StageNumber = 3,
                 Id = "8239e81a-f779-4c7c-b586-cd5ad732f061",
