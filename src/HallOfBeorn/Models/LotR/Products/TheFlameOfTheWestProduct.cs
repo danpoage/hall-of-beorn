d78c052456ed29683e0933c3a7c0785e4c2e8e26
Dan Poage <dan.poage@gmail.com>
2017-03-04 14:34:18 -0600

    Added RulesUrl to TheFlameOfTheWestProduct

--- src/HallOfBeorn/Models/LotR/Products/TheFlameOfTheWestProduct.cs
+++ src/HallOfBeorn/Models/LotR/Products/TheFlameOfTheWestProduct.cs
@@ -15,6 +15,8 @@ namespace HallOfBeorn.Models.LotR.Products
             AddScenario(new HallOfBeorn.Models.LotR.Scenarios.ThePassingOfTheGreyCompany());
             AddScenario(new HallOfBeorn.Models.LotR.Scenarios.TheSiegeOfGondor());
             AddScenario(new HallOfBeorn.Models.LotR.Scenarios.TheBattleOfThePelennorFields());
+
+            RulesUrl = "https://images-cdn.fantasyflightgames.com/filer_public/e8/5c/e85c8012-6840-432b-bd81-ac7792aa6281/mec54_rulesheet.pdf";
         }
     }
 }
