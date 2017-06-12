1b59dbc7ab11096bd53606ba86ec72f08ce5bb3d
Dan Poage <dan.poage@gmail.com>
2017-06-12 03:54:19 -0500

    Created AShadowOfThePast and WeMustAwayEreBreakOfDay encounter sets

---
+++ src/HallOfBeorn/Models/LotR/EncounterSets/TheHobbit/WeMustAwayEreBreakOfDay.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HallOfBeorn.Models.LotR.EncounterSets.TheHobbit
+{
+    public class WeMustAwayEreBreakOfDay : EncounterSet
+    {
+    }
+}
