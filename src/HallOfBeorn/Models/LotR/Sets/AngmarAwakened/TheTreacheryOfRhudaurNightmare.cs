43c7f426676737f9845f446021003041f3b12a70
Dan Poage <dan.poage@gmail.com>
2017-06-16 00:26:03 -0500

    Fixed Sift through the Ruins, Decipher Ancient Texts and Quiet the Spirits encounter sets in TheTreacheryOfRhudaurNightmare

--- src/HallOfBeorn/Models/LotR/Sets/AngmarAwakened/TheTreacheryOfRhudaurNightmare.cs
+++ src/HallOfBeorn/Models/LotR/Sets/AngmarAwakened/TheTreacheryOfRhudaurNightmare.cs
@@ -8,6 +8,9 @@ namespace HallOfBeorn.Models.LotR.Sets.AngmarAwakened
     public class TheTreacheryOfRhudaurNightmare : CardSet
     {
         private const string setName = "The Treachery of Rhudaur Nightmare";
+        private const string setDecipherAncientTexts = "Decipher Ancient Texts";
+        private const string setSiftThroughTheDebris = "Sift through the Debris";
+        private const string setQuietTheSpirits = "Quiet the Spirits";
 
         protected override void Initialize()
         {
@@ -18,22 +21,8 @@ namespace HallOfBeorn.Models.LotR.Sets.AngmarAwakened
             Number = 2078;
 
             addCard(LotRCard.NightmareSetup(setName, setName)
-                .WithText("You are playing Nightmare mode.\r\nSetup: Set the Sift the Debris, Decipher Ancient Texts and Quiet the Spirits encounter sets aside, out of play. THese sets are indicated by the following icons, respectively:\r\n\r\nForced: After you advance to stage 2b, for each of the above sidequests that was not defeated, shuffle the encounter set that matches its title into the encounter deck, and shuffle the encounter discard pile into the encounter deck.")
-                .WithOppositeText(
-@"Begin with the standard quest deck and encounter deck for The Treachery of Rhudaur scenario.
-
-Remove the following cards, in the specified quantities, from the standard encounter deck:
-
-2x Decrepit Ramains
-2x Eerie Halls
-1x Dark Covenant
-2x Ruins of Arnor
-2x Tragic Discovery
-5x Cursed Dead
-
-Then, shuffle the encounter cards in this Nightmare Deck into the remainder of the standard The Treachery of Rhudaur encounter deck.
-
-Finally, flip this setup card over and place it next to the quest deck. Its effect remains active throughout the game, which is now ready to begin.")
+                .WithText("You are playing Nightmare mode.\r\nSetup: Set the Sift through the Debris, Decipher Ancient Texts and Quiet the Spirits encounter sets aside, out of play. These sets are indicated by the following icons, respectively:\r\n\r\nForced: After you advance to stage 2b, for each of the above sidequests that was not defeated, shuffle the encounter set that matches its title into the encounter deck, and shuffle the encounter discard pile into the encounter deck.")
+                .WithOppositeText("Begin with the standard quest deck and encounter deck for The Treachery of Rhudaur scenario.\r\n\r\nRemove the following cards, in the specified quantities, from the standard encounter deck:\r\n\r\n2x Decrepit Ramains\r\n2x Eerie Halls\r\n1x ~Dark Covenant\r\n2x ~Ruins of ~Arnor\r\n2x Tragic Discovery\r\n5x Cursed Dead\r\n\r\nThen, shuffle the encounter cards in this Nightmare ~Deck into the remainder of the standard The Treachery of Rhudaur encounter deck.\r\n\r\nFinally, flip this setup card over and place it next to the quest deck. Its effect remains active throughout the game, which is now ready to begin.")
                 .WithInfo(1, 1, Artist.Unknown));
             addCard(LotRCard.Enemy("Thaurdir's Hunter", string.Empty, setName, 16, 2, 2, 1, 6)
                 .WithTraits("Undead.")
@@ -57,16 +46,16 @@ Finally, flip this setup card over and place it next to the quest deck. Its effe
                 .WithText("When Revealed: Remove the questing character with the higest Willpower from the quest. The player who controls that character raises his threat by an amount equal to that character's Willpower.")
                 .WithShadow("Shadow: If this attack destroys a character, heal all damage from each Undead enemy in play.")
                 .WithInfo(6, 2, Artist.Guillaume_Ducos));
-            addCard(LotRCard.Enemy("Apostate of Angmar", string.Empty, setName, 38, 5, 2, 2, 12)
+            addCard(LotRCard.Enemy("Apostate of Angmar", string.Empty, setQuietTheSpirits, 38, 5, 2, 2, 12)
                 .WithTraits("Undead.")
                 .WithText("While you are engaged with Apostate of Angmar, you cannot attack Thaurdir.")
                 .WithShadow("Shadow: Discard the top card of the encounter deck. Attacking enemy gets +X Attack, where X is that card's printed threat.")
                 .WithInfo(7, 3, Artist.Unknown));
-            addCard(LotRCard.Location("Secret Antechamber", string.Empty, setName, 2, 5)
+            addCard(LotRCard.Location("Secret Antechamber", string.Empty, setSiftThroughTheDebris, 2, 5)
                 .WithTraits("Ruins.")
                 .WithText("Immune to player card effects.\r\nWhile Secret Antechamber is in the staging area, no more than 5 progress can be placed on the main quest each round.\r\nTravel: Raise each player's threat by 3 to travel here.")
                 .WithInfo(8, 3, Artist.Federico_Musetti));
-            addCard(LotRCard.Treachery("Life Drain", string.Empty, setName)
+            addCard(LotRCard.Treachery("Life Drain", string.Empty, setDecipherAncientTexts)
                 .WithTraits("Sorcery.")
                 .WithText("When Revealed: Deal Life Drain to Thaurdir, as a shadow card.")
                 .WithShadow("Shadow: If attacking enemy is Thaurdir, he gets +3 for this attack. If this attack destroys a character, attach Life Drain to Thaurdir. (Counts as a Condition attachment with the text: \"Thaurdir gets +3 hit points.\")")
