75cd093c598792d82e82c1f9a0a76a1b2f0e0626
Dan Poage <dan.poage@gmail.com>
2017-08-14 18:25:39 -0500

    Fixed the Bio for AragornCharacter, ArwenUndomielCharacter, BeornCharacter, BeregondCharacter and BifurCharacter to use TolkienGateway instead of Wikipedia

--- src/HallOfBeorn/Models/LotR/Characters/BeornCharacter.cs
+++ src/HallOfBeorn/Models/LotR/Characters/BeornCharacter.cs
@@ -11,19 +11,28 @@ namespace HallOfBeorn.Models.LotR.Characters
             : base("Beorn", "Beorn", "Skin-Changer")
         {
             Bio =
-@"<p>Beorn is a fictional character created by J. R. R. Tolkien. He appears in The Hobbit as a 'skin-changer', a man who could assume the form of a great black bear.</p>
-<h4>The Hobbit</h4>
-<p>The Man named Beorn lived with his animal retinue (horses, dogs, sheep, and cows, among others) in a wooden house between the Misty Mountains and Mirkwood, to the east of the Anduin. According to Gandalf, Beorn 'does not eat them; neither does he hunt or eat wild animals'.[1] Gandalf believed that Beorn was either a descendant of the bears who had lived in the Misty Mountains before the arrival of the giants, or he was a descendant of the men who had lived in the region before the arrival of the dragons or Orcs from the north.</p>
-<p>Beorn was of immense size and strength for a man, and retained his size and strength in bear-form. He had black hair (in either form) and a thick black beard and broad shoulders (in human form). While not a 'giant' outright, Beorn's human form was of such great size that the three and a half foot tall Bilbo judged that he could have easily walked between Beorn's legs without touching his body. Beorn also named the Carrock and created the steps that led from its base to its flat top.</p>
-<p>In The Hobbit, Beorn received Gandalf, Bilbo Baggins, and 13 Dwarves, and aided them in their quest to reclaim their kingdom beneath the Lonely Mountain. He was convinced of their trustworthiness after confirming their tale of encountering the Goblins of the Misty Mountains and Gandalf's slaying of their leader, the Great Goblin. In addition to giving the group much-needed supplies and lodging, Beorn gave them vital information about what path to take while crossing Mirkwood.</p>
-<p>Later, hearing of a vast host of Goblins on the move, Beorn arrived at the Lonely Mountain in time to strike the decisive blow in the Battle of Five Armies. In his bear form he slew the Goblin leader, Bolg and his bodyguards. Without direction, the Goblin army scattered and were easy pickings for the other armies of Men, Elves, Dwarves, and Eagles. Beorn often left his home during the narrative of The Hobbit for hours or days at a time, for purposes not completely explained.</p>
-<p>Later in the Hobbit, it is said that 'Beorn indeed became a great chief afterwards in those regions and ruled a wide land between the mountains and the wood; and it is said that for many generations the men of his line had the power of taking bear's shape, and some were grim men and bad, but most were in heart like Beorn, if less in size and strength.' (The Hobbit Chapter 18 The Return Journey). It appears the ability to change shape into a bear was not exclusive to Beorn as an individual and may, in fact, have been an ability of a small sub-set within the race of Men.</p>
-<h4>The Lord of the Rings</h4>
-<p>In the years between the Battle of Five Armies and the War of the Ring, possibly spurred by his interaction with Thorin's company, Beorn emerged from his reclusion, and rose to become a leader of the woodmen living between the Anduin river and the fringes of Mirkwood. As stated by Glóin in The Fellowship of the Ring, the Beornings also 'keep open the High Pass and the Ford of Carrock.'</p>
-<p>Some time before the War of the Ring itself began, Beorn was succeeded by his son Grimbeorn the Old. His death is not included in the chronologies in The Return of the King's appendices.</p>
-<h4>Concept and creation</h4>
-<p>In naming his character, Tolkien used beorn, an Old English word for man and warrior (with implications of freeman and nobleman in Anglo-Saxon society).[2] It is related to the Scandinavian names Björn (Swedish and Icelandic) and Bjørn (Norwegian and Danish), meaning bear; and the figure of Beorn can be related to the traditional Northern heroes Bodvar Bjarki and Beowulf.</p>";
-            BioSourceUrl = "https://en.wikipedia.org/wiki/Beorn";
+@"<p>Beorn was a Skin-changer, a man who could assume the form of a bear.</p>
+<h4>History</h4>
+<p>His origins lay in the distant past, and Gandalf suspected he and his people had originally come from the mountains, until the Orcs of the Misty Mountains drove them away. He lived with his tame horses in a wooden house (Beorn's Hall) between the Misty Mountains and Mirkwood, to the east of the Great River of Wilderland.</p>
+<p>Beorn named the Carrock and created the steps that led from its base to the flat top. Once Gandalf saw him as a bear sitting all alone on the top of the Carrock watching the moon sinking towards the Misty Mountains, and heard him growl in the tongue of bears 'The day will come when they will perish and I shall go back!'. While Gandalf knew him, Beorn did not know Gandalf; but he knew his cousin, Radagast.</p>
+<p>During the Quest of Erebor, Beorn received Gandalf, Bilbo Baggins and the thirteen Dwarves and gave the Dwarves and Bilbo help in their quest.</p>
+<p>In the Battle of Five Armies, Beorn appeared transformed into a giant bear, rescued Thorin from the Goblins and killed their leader, Bolg.</p>
+<p>After Thorin's burial, Beorn followed Bilbo, Gandalf and Thranduil on their way back to the west. Beorn once more hosted Bilbo and Gandalf and other Woodmen who came to celebrate Yule.</p>
+<p>Eventually, he became a 'great chief' in the Vales of Anduin, and it is said that his descendants also were skin-changers, able to take the shape of a bear.[2] His people became known as the Beornings, and they helped defend Thranduil's kingdom of northern Mirkwood.</p>
+<p>Beorn presumably died some time before the War of the Ring began[5] and was succeeded by his son Grimbeorn the Old.</p>
+<h4>Character</h4>
+<p>Beorn was generally benevolent and hated goblins and wargs; but he was also a loner and distrustful of travelers and beggars. He was never polite, and became easily angry and appalling. He never invited people into his house and his very few friends, who lived a good way away, came no more than a couple to his house at a time. Added to this, Beorn was not fond of Dwarves.</p>
+<p>Although fierce to his enemies, once convinced of the others' goodwill, he was welcoming, generous and helpful.</p>
+<p>He did not eat any meat, as he could talk with his animals, who helped him. His grove was occupied by huge bees. Beorn's diet was mainly honey and cream.</p>
+<h4>Etymology</h4>
+<p>Beorn is an Old English word meaning 'a warrior, a hero, a man of valour' (also, poetic 'man'), cognate to Old Norse björn ('bear'). In the Scandinavian-speaking countries Björn/Bjørn is a personal name, attested since the 11th century.</p>
+<h4>Other versions of the Legendarium</h4>
+<p>In early manuscripts of The Hobbit, the name of the character that would become Beorn is Medwed. Medwed's ability to change shape to a bear was due to an enchantment, perhaps of his own.[10] The similarity between Medwed and Medved, the Russian word for 'bear', is striking, but it is ultimately unknown if Tolkien had this in mind.</p>
+<p>In the Etymologies, Ilkorin ber ('valiant man, warrior') and Danian beorn ('man') derive from the Elvish root BER.</p>
+<h4>Inspiration</h4>
+<p>It has been suggested that the character Beorn was influenced by the Norse Saga of Hrólfr Kraki.[12] In the saga appears a great bear defending Hrólfr Kraki, and also a man cursed to being transformed into a bear during nighttime. The work was well-known to J.R.R. Tolkien, as one of his students and friend, Stella Miller, made a translation of the saga dedicated to Tolkien, E.V. Gordon and C.T. Onions;[7] but even more importantly as Tolkien himself wrote an unpublished short story of the tale, entitled Sellic Spell.</p>
+<p>Another suggested inspiration is the character of Beowulf, 'whose name is commonly explained as Beowulf = 'bees' wolf' = honey-eater = bear, and one who breaks swords, rips off arms and cracks ribs with ursine power and clumsiness.'</p>";
+            BioSourceUrl = "http://tolkiengateway.net/wiki/Beorn";
             ImageArtist = Artist.Joel_M_Kilpatrick;
 
             Alias("Chieftain of the Beornings");
--- src/HallOfBeorn/Models/LotR/Characters/BeregondCharacter.cs
+++ src/HallOfBeorn/Models/LotR/Characters/BeregondCharacter.cs
@@ -11,12 +11,28 @@ namespace HallOfBeorn.Models.LotR.Characters
             : base("Beregond", "Beregond", "Man")
         {
             Bio =
-@"<p>Beregond is a fictional characters in J. R. R. Tolkien's Middle-earth legendarium, appearing in The Return of the King.<p>
-<p>Beregond is the first Captain of the White Company; the guard of Faramir, Prince of Ithilien; and previously, a member of the Third Company of the Guard of the Citadel in Minas Tirith. He is appointed to this rank after he saves Faramir's life during the Siege of Minas Tirith. In the novel, he and his son Bergil were also noted for being the guides of Pippin Took in Minas Tirith. The two form a deep friendship and talk about their various troubles. In later battle, Beregond and Pippin are stunned and crushed by the bodies of oncoming enemies. Both are saved when Gimli notices the familiar shape of Pippin's foot and digs for him. He has two children, Bergil and Borlas.</p>
-<p>During the Siege of Gondor, Beregond received news from Pippin that Denethor intended to kill both himself and the seemingly-dead Faramir. Leaving his post, he fought the door guards of the place where Denethor had taken Faramir in order to reach him, until the arrival of Gandalf and Pippin, whereupon Faramir was saved. After protecting Faramir, he carried him to the Houses of Healing and stood guard over him until he awoke.</p>
-<p>Beregond traveled with Aragorn's army to the Gates of Morannon to challenge the forces of Sauron. They served under Prince Imrahil, and during the Battle of the Morannon, Beregond was nearly killed by a Troll.</p>
-<p>After the War of the Ring, Beregond was brought before the newly crowned Aragorn II Elessar to answer for the abandonment of his post, as well as the murders of those who stood in his way as he raced to rescue Faramir. Recognizing that what he did was out of love for his lord, he did not impose the death penalty upon him; the newly crowned king's largesse allowed for Beregond to simply be banished from Minas Tirith and live in Ithilien to serve Faramir, for whom he had broken the laws to rescue. He was then promoted to the rank of Captain of the White Company.</p>";
-            BioSourceUrl = "https://en.wikipedia.org/wiki/Beregond_and_Bergil";
+@"<blockquote>&ldquo;I am no captain. Neither office nor rank nor lordship have I, being but a plain man of arms of the Third Company of the Citadel.&rdquo;
+<br>― Beregond
+</blockquote>
+<p>Beregond, son of Baranor, was a soldier of Gondor and later Captain of the White Company.</p>
+<h3>History</h3>
+<h4>Early Life</h4>
+<p>Beregond's fathers were from the vales of the White Mountains, and prior to that they lived in Ithilien.[1] Beregond had two sons, Bergil, who was ten years of age at the time of the War of the Ring,[1] and a younger son named Borlas.</p>
+<p>Beregond was a member of the Third Company of the Citadel. He admired Faramir and saw him as a brave warrior and a great leader. Beregond did not travel outside of Minas Tirith often but understood the threat that Sauron posed to the city.</p>
+<h4>War of the Ring</h4>
+<p>On the morning of March 9th, T.A. 3019, Beregond was assigned to help Peregrin Took who had just become a Guard of the Citadel. As Beregond had never met a hobbit before, he was eager to meet Pippin and he was astounded that Pippin looked much like a child yet had faced more dangers than most Men of Gondor.</p>
+<h4>Battle of Pelennor Fields</h4>
+<p>During the Battle of Pelennor Fields, Beregond was on watch as the sentinel at the gate of the Citadel. Pippin rushed to tell him that Denethor was attempting to burn Faramir alive. Beregond knew his duty of protecting Faramir was more important than to remain at his post so he rushed to his Captain at the Silent Street. The guard to the street would not let him pass and Beregond was forced to kill him so he could enter. He regretted his decision but he was forced to act quickly. Two more servants of Denethor's guard were killed as they attempted to light the funeral pyre. He stood upon the steps with his sword to ward off the rest until Gandalf arrived. After Faramir was rescued, Denethor would still not give up, and drew a knife to attempt to kill his son, but Beregond stepped forward to block Denethor. Denethor then gave up and set himself on fire and died.</p>
+<p>After helping to carry Faramir to the Houses of Healing, Gandalf gave the key to the Closed Door to the injured Faramir. Beregond stood guard over Faramir at the Houses of Healing and was present when he awoke.</p>
+<h4>Battle of the Morannon</h4>
+<p>Pippin and Beregond served under the command of Imrahil at the Battle of the Morannon. When a Troll-chief charged their ranks, Beregond was stunned and nearly killed, but Pippin managed to save his friend by killing the Troll, which fell on top of him.</p>
+<h4>Later Life</h4>
+<p>Upon Aragorn's coronation on May 1st, Beregond was brought forward to receive judgement for leaving his post and for killing Denethor's guards on hallowed ground. The penalty would have been death, but the King showed mercy as the deeds were done out of love for Faramir, and Beregond was merely forbidden from returning to the White City ever again. For his bravery in battle, Aragorn appointed him the Captain of the Guard of Faramir, which was also called the White Company. He later went to live in Emyn Arnen in Ithilien to serve his lord, Faramir, the new Prince of Ithilien.</p>
+<h4>Etymology</h4>
+<p>'Beregond' may stem from beren meaning 'bold' and gond meaning 'stone'. He was likely named after the steward bearing the same name.</p>
+<h4>Genealogy</h4>
+<p>Beregond was descended from people of the vales of the White Mountains and Ithilien. What exactly his relation to Iorlas, described only as an uncle of Bergil,[1] was, is unknown.</p>";
+            BioSourceUrl = "http://tolkiengateway.net/wiki/Beregond";
             ImageArtist = Artist.Magali_Villeneuve;
 
             addFamily("Bergil (Son)");
