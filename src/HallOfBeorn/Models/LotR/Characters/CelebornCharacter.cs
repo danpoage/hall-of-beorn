ecf3cba170395a0a0db9b57f8b88fb88dbc67856
Dan Poage <dan.poage@gmail.com>
2017-08-15 15:15:59 -0500

    Fixed Bio for BoromirCharacter, CelebornCharacter and CirdanTheShipwrightCharacter to use TolkienGateway instead of Wikipedia

--- src/HallOfBeorn/Models/LotR/Characters/CelebornCharacter.cs
+++ src/HallOfBeorn/Models/LotR/Characters/CelebornCharacter.cs
@@ -11,16 +11,54 @@ namespace HallOfBeorn.Models.LotR.Characters
             : base("Celeborn", "Celeborn", "Sindarin Elf")
         {
             Bio =
-@"<p>Celeborn (pronounced with a hard c as in cat; IPA: [ˈkelebɔːn]) is a fictional character in J.R.R. Tolkien's Middle-earth legendarium. Celeborn is an Elf, and the husband of Galadriel. He appears in The Lord of the Rings as the Lord of the Galadhrim, and co-ruler along with Galadriel of Lothlórien. He was the father of Celebrían (the wife of Elrond), and thus the grandfather of Arwen Evenstar and her older brothers Elladan and Elrohir. He was also a kinsman of the First Age Elven King Thingol. Celeborn means '(tall) silver tree' in Tolkien's invented language of Sindarin.</p>
-<p>He is also referred to as Lord of Lothlórien or Lord of the Wood interchangeably. Within Lothlórien itself he is called simply Lord Celeborn or The Lord (more often with Galadriel as The Lord and Lady). He is called Celeborn the Wise by Galadriel.</p>
-<p>Celeborn is also the name of the White Tree that flourished in Tol Eressëa. It was a seeding of the tree Galathilion, which in turn was made by Yavanna in the image of Telperion, the elder of the Two Trees of Valinor. Celeborn was an ancestor to the White Trees of Númenor and of Gondor.</p>
-<p>Celeborn first appears in The Fellowship of the Ring. He also appears briefly in Tolkien's (at the time unpublished) existing legends of the First Age, The Silmarillion; in chapters 13, 22, 24 and also 'Of the Rings of Power and the Third Age'. Tolkien felt that an Elf of Celeborn's importance should have noble ancestry, and spent several years trying to resolve his origins.</p>
-<p>Most existing writings portray Celeborn as a Sindarin Elf from Doriath, the kingdom of Thingol. In one speculative text by Tolkien, published in Unfinished Tales,[3] Celeborn is the grandson of Thingol's younger brother Elmo (who appears nowhere else). That Celeborn and Thingol are related is suggested by their silver hair, a colour associated with the House of Elwë and Olwë and rarely mentioned outside of the royal house of the Sindar. Míriel Serindë, mother of Fëanor, also has silver hair, as did Eärwen, daughter of Olwë and mother of Galadriel. This Sindarin origin for Celeborn was adopted by Christopher Tolkien for The Silmarillion, which describes Celeborn as 'a kinsman of Thingol' and adds a paragraph about Celeborn's marriage to Galadriel.</p>
-<p>Late in his life Tolkien proposed a different scenario (also described in Unfinished Tales): Celeborn was a Telerin Elf from Valinor, the grandson of Olwë, named Teleporno in Telerin. (This brings him closer to being a peer and equal of Galadriel and makes their partnership more balanced.) He met Galadriel when she was living in the Telerin city Alqualondë, before the rebellion of the Noldor. They decided to travel together to Middle-earth, without asking permission from the Valar to do so. They made their way to Middle-earth independently of Fëanor, and in some versions of the story Galadriel fell under the Ban of the Valar, in other versions not. Tolkien never mentions the Ban applying to Celeborn. According to this narrative, the name Teleporno was 'Sindarized' as Celeborn: Telerin telpe ('silver') (Noldorin Quenya tyelpe) was translated to Sindarin keleb (or celeb).</p>
-<p>This altered origin was not adopted in the published Silmarillion, primarily because it would have necessitated a major rewrite of the earlier parts to account for a separate departure of Galadriel. A further problem was that this descent would have made Celeborn Galadriel's first cousin, and Elves never married close kin.</p>
-<p>Early in the Second Age, Celeborn apparently ruled over Elves in Lindon. Celeborn and Galadriel later rule over the Elves of Eregion (at least in some accounts), before settling in Lothlórien.</p>
-<p>At the end of the Third Age Celeborn crossed the Anduin with many elves of Lothlórien to found 'East Lórien'. When Galadriel left Middle-earth shortly afterward, Celeborn settled in Rivendell to join his grandsons Elladan and Elrohir. The prologue to The Fellowship of the Ring states that 'there is no record of the day when at last he sought the Grey Havens, and with him went the last living memory of the Elder Days in Middle-earth'.</p>";
-            BioSourceUrl = "https://en.wikipedia.org/wiki/Celeborn";
+@"<p>Celeborn (S: 'silver-tall', pron. [ˈkeleborn]) was a noble Elf - a member of the Sindar[3] - who wedded Galadriel of the Noldor. After crossing the Blue Mountains at the beginning of the Second Age and living in the realm of Eregion for a while, he and Galadriel moved to Lothlórien to become the Lord and Lady of Lórien. His daughter Celebrían married Elrond Half-elven whose daughter Arwen chose a mortal life to wed Aragorn.</p>
+<h3>History</h3>
+<h4>Early Life</h4>
+<p>Celeborn was a Sindarin prince from Doriath, the grandson of Elmo (the brother of Elwe/Thingol)</p>
+<h4>First Age</h4>
+<p>In the early Years of the Sun Finrod and Galadriel came to Doriath as guests of Elu Thingol, the King of Doriath. There Celeborn and Galadriel met, fell in love and were wedded. Galadriel remained in Doriath with Celeborn after Finrod went to the Caverns of Narog to establish the stronghold of Nargothrond.</p>
+<p>For the rest of the First Age Celeborn and Galadriel are not mentioned to have played any significant role in the general course of events of the Age, while their relatives, both Sindarin and Noldorin did. The only thing that can be said for sure, is that the couple survived the Wars of Beleriand, including the Second Kinslaying by the Sons of Feanor[5]. They may have escaped to the Havens or the Isle of Balar.</p>
+<h4>Second Age</h4>
+<p>After the fall of Beleriand, Celeborn and Galadriel came to Lindon the coastal region west of the Blue Mountains. Celeborn ruled the fief of Harlindon, which was composed mostly of Sindar, under the High King Gil-galad. They eventually came to Eregion where they dwelt for a time with Celebrimbor and the jewelsmiths of Gwaith-i-Mírdain. After Celebrimbor had largely taken over, they made contact with the Nandor of Lindórinand, across the Hithaeglir. Not long after they went through Khazad-dûm to live there, becoming great among its people.</p>
+<p>It is said that Celeborn fought in the Sack of Eregion, and he and Elrond narrowly escaped to a dell, where the latter founded Rivendell.</p>
+<p>After the Sack of Eregion, Celeborn and Galadriel came to Lindórinand, the land of Silvan Elves which was ruled by King Amdir. When Amdir died in the Battle of Dagorlad, his son Amroth succeeded him.</p>
+<h4>Third Age</h4>
+<p>In early Third Age Amroth was weary of Middle-earth and wished to sail to the West. He departed to Edhellond and by the year T.A. 1981, King Amroth drowned in the Bay of Belfalas. Celeborn and Galadriel took the title Lord and Lady of Lothlórien, the new name for Lindórinand and together they built Caras Galadhon.</p>
+<p>Celeborn and Galadriel had a daughter named Celebrían,[note 2] who later married Elrond Half-elven of Rivendell, thus making Celeborn and his wife Galadriel the grandparents of the twins Elladan and Elrohir and their younger sister Arwen Undómiel.</p>
+<p>In response to the growing threat of Dol Guldur, the White Council was formed in T.A. 2463. Galadriel was on it; it is likely that Celeborn was also a member.</p>
+<p>During the War of the Ring in T.A. 3019, Lórien received the Fellowship of the Ring, composed of various travelers on the quest to destroy the One Ring. He offered advice, and boats for the Anduin, speeding them on their way.</p>
+<p>Celeborn's most significant recorded actions came in the same year (in March 3019), when he repulsed three attacks by orcs of Dol Guldur. Celeborn then marshalled his forces to cross the Anduin and lay siege to Dol Guldur. He captured the fortress, and Galadriel threw down its walls and purified it.</p>
+<h4>After the War of the Ring</h4>
+<p>Celeborn attended the wedding of King Elessar and his granddaughter Arwen, and on the journey to return bade a fond farewell to Treebeard as well.</p>
+<blockquote>
+&ldquo;I do not think we shall meet again.&rdquo;<br>
+&ldquo;I do not know, Eldest.&rdquo;<br>
+― Treebeard and Celeborn, Many Partings.
+</blockquote>
+<p>Celeborn returned to Lothlórien, but the power of Nenya was broken. It no longer was filled with the light and life it had before. Galadriel shortly after passed West with the other Ring-bearers, and Celeborn, weary, moved to Imladris to live with Elladan and Elrohir, Elrond’s sons. After Círdan's departure Celeborn was the last of the Wise in Middle-earth, they that had seen the struggles of Elvenkind. It is not recorded when he sought the Grey Havens and sailed west, but when he did so he took with him the last memory of the Elder Days in Middle-earth.</p>
+<h4>Character</h4>
+<p>What can be gleaned of his personality is that he was quick-thinking and swift to act, one of the wisest elves of Middle-earth in the Third Age. He was a warrior too, leading his followers into battle against Dol Guldur and other menaces.</p>
+<h4>Other Versions of the Legendarium</h4>
+<p>According to an alternate history of Celeborn and Galadriel in Unfinished Tales, Celeborn was a Teler of noble rank in Alqualondë, born Teleporno, grandson of Olwë, King of the Falmari.</p>
+<p>Artanis, Olwë's granddaughter and a Noldorin princess of great beauty, often spent long visits to Alqualondë and her mother’s kin. During this time the two fell in love, and Teleporno gave her the romantic epithet Alatáriel, meaning 'Maiden Crowned with Radiant Garland'. Teleporno's name gained an additional Quenya form, Telporno (pron. [telˈporno]).</p>
+<p>Later, Teleporno and Artanis helped to defend Alqualondë from the Noldor during the First Kinslaying. Nevertheless, Artanis’s heart was fired by the stories of Fëanor of the open lands across the sea. She urged Teleporno to take a ship eastward to Endor, and he gave in. In doing so the two fell under the Ban of the Valar.</p>
+<p>In Beleriand, Teleporno and Artanis were greeted by Elu Thingol, High King of the Sindar and Lord of Doriath. Teleporno eventually Sindarinized his name to Celeborn; Artanis Sindarinized the epithet Celeborn had given her, and Alatáriel became Galadriel.</p>
+<h4>Controversy</h4>
+<p>Up to the time when The Lord of the Rings was published, there had been no mention of Celeborn in the previously written legendarium. Tolkien later attempted multiple times to fix upon the story behind such an important elf. He remains a fairly unsubstantial character in the legendarium, for despite his profound effect upon the doings of the Elder Days, there is very little direct information on him as a person.</p>
+<p>In his first writings (which make up the bulk of the matter of Celeborn)and the works published during Tolkien's lifetime, the elf-lord is portrayed as a kinsman of Thingol and one of the Sindar,[9][10] and met Galadriel when she came with the Noldor into exile. This was the approach chosen by Christopher Tolkien and Guy Gavriel Kay for The Silmarillion. In a brief, speculative paper written by Tolkien included in Unfinished Tales, Celeborn is made Thingol’s grandnephew, by the King’s brother Elmo. Elmo, also linked to Círdan, appears nowhere else in the legendarium.</p>
+<p>A Sindarin origin as Elmo's grandson has been the most widely accepted one of all, but Celeborn as a Telerin prince seems a late development. It's unknown whether Tolkien would have revised the story or would have felt bound by published writings.</p>
+<p>In Tolkien's last writings Celeborn is a Falmarin Elf from Valinor, then named Teleporno in Telerin.[11] The name Teleporno was then 'Sindarized' as Celeborn. In this account Celeborn is stated to have been a grandson of the Telerin king Olwë, so that he still was a kinsman of the Sindarin king Thingol.</p>
+<p>This altered origin was not adopted into the published Silmarillion, primarily because it contradicted canon and was not incorporated in Tolkien's drafts of the earlier parts, and therefore would have necessitated a major rewrite by C. Tolkien and Kay to account for a separate departure of Galadriel.</p>
+<blockquote>
+&ldquo;On the other hand, the making of Celeborn into a Telerin Elf of Aman contradicts not only statements in The Silmarillion, but also those cited already from The Road Goes Ever On and Appendix B to The Lord of the Rings&rdquo;
+<br>― Christopher Tolkien, Unfinished Tales
+</blockquote>
+<p>A further problem was that this descent would have made the couple first or second cousin (assuming that he was still to be considered closely related to Thingol), and Elves never married close kin.</p>
+<p>However, in the Morgoth's Ring it is stated that the first cousins might marry, but seldom did so, or desired to do so.</p>
+<h4>Etymology</h4>
+<p>Celeborn is a Sindarin name, consisting of celeb ('silver') + a derivative of ornā ('tall'). Teleporno, his original Telerin name, has an identical meaning. The name Teleporno was 'Sindarized' as Celeborn: Telerin telpe ('silver') (Quenya tyelpe) was transformed to Sindarin celeb.</p>
+<p>He was given the epithet 'the Wise' by Galadriel in The Lord of the Rings.</p>";
+            BioSourceUrl = "http://tolkiengateway.net/wiki/Celeborn";
             ImageArtist = Artist.Magali_Villeneuve;
 
             TheFellowshipOfTheRing();
@@ -40,6 +78,7 @@ namespace HallOfBeorn.Models.LotR.Characters
             addFamily("Elrohir (Grandson)", "Elrohir");
             addFamily("Arwen Undómiel (Granddaughter)", "Arwen");
 
+            addFriend("Gil-galad");
             addFriend("Haldir of Lórien", "Haldir-of-Lorien");
             addFriend("Orophin", "Orophin");
             addFriend("Rúmil", "Rumil");
