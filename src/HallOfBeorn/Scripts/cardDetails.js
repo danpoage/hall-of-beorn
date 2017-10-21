71813125b18f6b46629c945c18ed0c4d47ff2dc1
Dan Poage <dan.poage@gmail.com>
2017-10-21 01:45:33 -0500

    Fixed cardDetails.js to handle the fact that jQuery is using deferred loading

--- src/HallOfBeorn/Scripts/cardDetails.js
+++ src/HallOfBeorn/Scripts/cardDetails.js
@@ -1,17 +1,19 @@
 ﻿var slug = window.location.href.split('/').pop(),
     url = '/Cards/TopDecks?slug=' + slug;
 
-$.ajax({
-    url: url,
-    type: "get",
-    success: function (results) {
-        if (results && results.length > 0) {
-            $('#deck-section').show();
-            var listHtml = '';
-            for (var i = 0; i < results.length; i++) {
-                listHtml += '<div><a title="' + results[i].name + '" href="' + results[i].url + '" target="_blank">' + results[i].name + '</a></div>';
+window.addEventListener('DOMContentLoaded', function () {
+    $.ajax({
+        url: url,
+        type: "get",
+        success: function (results) {
+            if (results && results.length > 0) {
+                $('#deck-section').show();
+                var listHtml = '';
+                for (var i = 0; i < results.length; i++) {
+                    listHtml += '<div><a title="' + results[i].name + '" href="' + results[i].url + '" target="_blank">' + results[i].name + '</a></div>';
+                }
+                $('#deck-list').html(listHtml);
             }
-            $('#deck-list').html(listHtml);
         }
-    }
+    });
 });
