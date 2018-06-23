7405f47d0217d321cecad5cbdd44c91726f16afc
Dan Poage <dan.poage@gmail.com>
2018-06-22 21:05:37 -0500

    Improvements to the StringBasicQueryFilter to prefer search results where entire tokens match the query

--- src/HallOfBeorn/Services/LotR/Search/StringBasicQueryFilter.cs
+++ src/HallOfBeorn/Services/LotR/Search/StringBasicQueryFilter.cs
@@ -18,21 +18,25 @@ namespace HallOfBeorn.Services.LotR.Search
             {
                 var points = 0f;
 
-                points += Matches((card, q) => card.Title.IsEqualToLower(q), query, score, 20);
-                points += Matches((card, q) => card.NormalizedTitle.IsEqualToLower(q), query, score, 20);
-                points += Matches((card, q) => card.Title.StartsWithLower(q), query, score, 14);
-                points += Matches((card, q) => card.NormalizedTitle.StartsWithLower(q), query, score, 14);
-                points += Matches((card, q) => card.Title.ContainsLower(q), query, score, 11);
-                points += Matches((card, q) => card.NormalizedTitle.ContainsLower(q), query, score, 11);
-                points += Matches((card, q) => card.Text.ContainsLower(q), query, score, 10);
-                points += Matches((card, q) => card.OppositeText.ContainsLower(q), query, score, 10);
-                points += Matches((card, q) => card.Traits.Any(tr => tr.ToLowerSafe().Equals(q)), query, score, 13);
-                points += Matches((card, q) => card.NormalizedTraits.Any(tr => tr.ToLowerSafe().Equals(q)), query, score, 13);
-                points += Matches((card, q) => card.Traits.Any(tr => tr.ToLowerSafe().Contains(q)), query, score, 12);
-                points += Matches((card, q) => card.NormalizedTraits.Any(tr => tr.ToLowerSafe().Contains(q)), query, score, 12);
-                points += Matches((card, q) => card.Keywords.Any(kw => kw.ToLowerSafe().Equals(q)), query, score, 15);
-                points += Matches((card, q) => card.Keywords.Any(kw => kw.ToLowerSafe().Contains(q)), query, score, 11);
-                
+                points += Matches((card, q) => card.Title.ToLowerSafe().Split(new char[]{' ', '-'}).Any(token => token.Equals(q)), query, score, 48);
+                points += Matches((card, q) => card.NormalizedTitle.ToLowerSafe().Split(new char[]{' ', '-'}).Any(token => token.Equals(q)), query, score, 48);
+                points += Matches((card, q) => card.Title.IsEqualToLower(q), query, score, 36);
+                points += Matches((card, q) => card.NormalizedTitle.IsEqualToLower(q), query, score, 36);
+                points += Matches((card, q) => card.Title.StartsWithLower(q), query, score, 24);
+                points += Matches((card, q) => card.NormalizedTitle.StartsWithLower(q), query, score, 24);
+                points += Matches((card, q) => card.Title.ContainsLower(q), query, score, 12);
+                points += Matches((card, q) => card.NormalizedTitle.ContainsLower(q), query, score, 12);
+                points += Matches((card, q) => card.Text.ContainsLower(q), query, score, 2);
+                points += Matches((card, q) => card.OppositeText.ContainsLower(q), query, score, 2);
+                points += Matches((card, q) => card.Traits.Any(tr => tr.ToLowerSafe().Equals(q)), query, score, 4);
+                points += Matches((card, q) => card.NormalizedTraits.Any(tr => tr.ToLowerSafe().Equals(q)), query, score, 4);
+                points += Matches((card, q) => card.Traits.Any(tr => tr.ToLowerSafe().Contains(q)), query, score, 2);
+                points += Matches((card, q) => card.NormalizedTraits.Any(tr => tr.ToLowerSafe().Contains(q)), query, score, 2);
+                points += Matches((card, q) => card.Keywords.Any(kw => kw.ToLowerSafe().Equals(q)), query, score, 4);
+                points += Matches((card, q) => card.Keywords.Any(kw => kw.ToLowerSafe().Contains(q)), query, score, 2);
+
+                score.AddScore(points);
+
                 return points > 0;
             };
         }
