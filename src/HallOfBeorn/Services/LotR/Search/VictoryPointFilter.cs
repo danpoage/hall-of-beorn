9f4757e8c02ba602eacb2b4c4d94d9780a0595fc
Dan Poage <dan.poage@gmail.com>
2018-06-23 00:06:10 -0500

    Added support for Victory>0 value in victory point filter

--- src/HallOfBeorn/Services/LotR/Search/VictoryPointFilter.cs
+++ src/HallOfBeorn/Services/LotR/Search/VictoryPointFilter.cs
@@ -12,6 +12,17 @@ namespace HallOfBeorn.Services.LotR.Search
             if (string.IsNullOrWhiteSpace(target) || target == defaultValue)
                 return;
 
+            if (target == "Victory>0")
+            {
+                predicate = (score) =>
+                {
+                    var match = score.Card.VictoryPoints > 0;
+                    score.AddScore(match ? 1 : 0);
+                    return match;
+                };
+                return;
+            }
+
             byte victoryPoints = 0;
             if (!byte.TryParse(target.Replace("Victory", string.Empty).Trim('.'), out victoryPoints))
                 return;
