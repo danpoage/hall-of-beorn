ce73ca4fb5896d79c2035091a7543f2cd69cc984
Dan Poage <dan.poage@gmail.com>
2018-07-19 23:02:12 -0500

    Added RingsDbCardLink implementation of ILink

---
+++ src/HallOfBeorn/Models/YouTube/RingsDbCardLink.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HallOfBeorn.Models.YouTube
+{
+    public class RingsDbCardLink : ILink
+    {
+        public RingsDbCardLink(string setId, string cardId, string title)
+        {
+            Title = title;
+            Text = title;
+            Url = string.Format("http://ringsdb.com/card/{0:00}{1:000}", setId, cardId);
+        }
+
+        public string Title { get; private set; }
+
+        public string Text { get; private set; }
+
+        public string Url { get; private set; }
+
+        public string ThumbnailUrl { get; private set; }
+
+        public int ThumbnailHeight { get; private set; }
+
+        public int ThumbnailWidth { get; private set; }
+    }
+}
