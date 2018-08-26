88b061fb17fd79adb2d36305bb86618229b33254
Dan Poage <dan.poage@gmail.com>
2018-08-25 23:59:38 -0500

    Added CardCycle enum

---
+++ src/HallOfBeorn/Models/LotR/CardCycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HallOfBeorn.Models.LotR
+{
+    public enum CardCycle
+    {
+        None = 0,
+        Shadows_of_Mirkwood,
+        Dwarrowdelf,
+        Agaist_the_Shadow,
+        The_Ring_maker,
+        Angmar_Awakened,
+        Dream_chaser,
+        Haradrim,
+        Ered_Mithrin,
+        GenCon,
+        The_Hobbit,
+        The_Lord_of_the_Rings,
+        NIGHTMARE,
+        Doom_Mastered,
+    }
+}
