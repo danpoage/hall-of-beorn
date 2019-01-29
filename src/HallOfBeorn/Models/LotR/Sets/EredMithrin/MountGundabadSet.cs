d2cb08ed5a32dad6e110f37befd723c13b1d5a2e
Dan Poage <dan.poage@gmail.com>
2019-01-29 00:38:37 -0600

    Fixed card text on Bilbo Baggins from Mount Gundabad Set

--- src/HallOfBeorn/Models/LotR/Sets/EredMithrin/MountGundabadSet.cs
+++ src/HallOfBeorn/Models/LotR/Sets/EredMithrin/MountGundabadSet.cs
@@ -21,7 +21,7 @@ namespace HallOfBeorn.Models.LotR.Sets.EredMithrin
 
             addHero("Bilbo Baggins", 9, Sphere.Tactics, 1, 1, 2, 2)
                 .WithTraits("Hobbit.", "Burglar.")
-                .WithText("Response: After Bilbo Baggins commits to the quest, choose an enemy in the staging area. Until the end of the phase, Bilbo Baggins gets +X Willpower where X is that enemy's Threat. If the players quest successfully, deal 1 damage to that enemy (2 damage instead if it is guarding a card).")
+                .WithText("Response: After Bilbo ~Baggins commits to the quest, choose an enemy in the staging area. Until the end of the phase, Bilbo ~Baggins gets +X Willpower where X is that enemy's Threat. If the players quest successfully, deal 1 damage to that enemy (2 damage instead if it is guarding a card).")
                 .WithInfo(112, 1, Artist.Tropa_Entertainment);
 
             addAlly("Gaffer Gamgee", 2, Sphere.Lore, true, 1, 1, 0, 1)
