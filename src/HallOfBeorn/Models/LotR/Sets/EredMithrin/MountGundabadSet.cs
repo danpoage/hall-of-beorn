7e9ccfe618df8c6d2d02b56f8fb459a8cd3cfc1d
Dan Poage <dan.poage@gmail.com>
2019-04-05 00:23:22 -0500

    Defined encounter cards for Mount Gundabad

--- src/HallOfBeorn/Models/LotR/Sets/EredMithrin/MountGundabadSet.cs
+++ src/HallOfBeorn/Models/LotR/Sets/EredMithrin/MountGundabadSet.cs
@@ -67,6 +67,81 @@ namespace HallOfBeorn.Models.LotR.Sets.EredMithrin
                 .WithText("Play only if each of your heroes is a Hobbit.\r\nAction: Reduce your threat by 4.")
                 .WithFlavor("\"If more of us valued food and cheer and song above hoarded gold, it would be a merrier world.\"\r\n-Thorin Oakenshield, The Hobbit")
                 .WithInfo(121, 3, Artist.Carmen_Cianelli);
+
+            addLocation("The First Forge", setName, 1, 1)
+                .WithUnique()
+                .WithTraits("Underground.")
+                .WithText("Immune to player card effects.\r\nForced: After The First Forge becomes the active location, exhaust the hero with Wormsbane attached. That hero cannot ready while The First Forge is the active location.\r\nResponse: After the First Forge is explored, flip Wormsbane to its Reforged side and attach it to any hero.")
+                .WithVictoryPoints(1)
+                .WithInfo(122, 1, Artist.Tomasz_Jedruszek);
+            addLocation("Dagnir's Hoard", setName, 7, 7)
+                .WithUnique()
+                .WithTraits("Undergound.")
+                .WithText("Forced: When Dagnir's Hoard is explored, Dagnir returns to the staging area and makes an immediate attack against each player in turn order.\r\nResponse: After Dagnir's Hoard is explored, each player may search his deck for an Item attachment, add it to his hand, and shuffle his deck.")
+                .WithVictoryPoints(1)
+                .WithInfo(123, 1, Artist.Nele_Diel);
+            addLocation("Throat of the Mountain", setName, 4, 4)
+                .WithUnique()
+                .WithTraits("Underground.", "Dark.")
+                .WithText("Response: When Throat of the Mountain is explored, search the quest deck for a quest card and make it the current main quest. Then, flip it over. Shuffle the rest of the quest deck.")
+                .WithFlavor("At the end of the hall the floor vanished and fell to an unknown depth.\r\n-The Fellowship of the Ring")
+                .WithVictoryPoints(1)
+                .WithInfo(124, 1, Artist.Lucas_Staniec);
+            addEnemy("Dagnir", setName, 50, Card.VALUE_X, 7, 5, 20)
+                .WithUnique()
+                .WithTraits("Dragon.")
+                .WithKeywords("Indestructible.")
+                .WithText("Immune to player card effects. X is 1 more than the number of quest cards in the victory display.\r\nCannot take more than X damage each round.\r\nWhile there are 5 or more quest cards in the victory display, Dagnir cannot leave the staging area and is considered to be engaged with each player.")
+                .WithTemplate("<p class='main-text'>{keyword:Indestructible.} Immune to player card effects. X is 1 more than the number of quest cards in the victory display.</p><p class='main-text'>Cannot take more than X damage each round.</p><p class='main-text'>While there are 5 or more quest cards in the victory display, {self} cannot leave the staging area and is considered to be engaged with each player.</p>")
+                .WithInfo(125, 1, Artist.Antonio_Jose_Manzanedo);
+            addEnemy("Guardian of Gundabad", setName, 39, 3, 4, 1, 5)
+                .WithTraits("Orc.", "Goblin.")
+                .WithText("While there are 3 or more quest cards in the victory display, ~Guardian of Gundabad gets +2 Defense.")
+                .WithShadow("Shadow: Attacking enemy gets +2 Defense until the end of the phase.")
+                .WithInfo(126, 3, Artist.Martin_de_Diego_Sadaba);
+            addEnemy("Dagnir's Slave", setName, 26, 2, 3, 2, 3)
+                .WithTraits("Orc.", "Goblin.")
+                .WithText("Dagnir cannot take damage from non-quest effects.")
+                .WithShadow("Shadow: Attacking enemy gets +1 Attack (+2 Attack instead if Dagnir is engaged with you).")
+                .WithEasyModeQuantity(2)
+                .WithInfo(127, 3, Artist.Rafal_Hrynkiewicz);
+            addEnemy("Deep-dweller", setName, 32, 2, 3, 1, 4)
+                .WithTraits("Creature.", "Spider.")
+                .WithText("Forced: When Deep-dweller engages you, it makes an immediate attack.\r\nForced: After Deep-dweller take any amount of damage (and survives), return it to the staging area.")
+                .WithEasyModeQuantity(1)
+                .WithInfo(128, 2, Artist.Guillaume_Ducos);
+            addLocation("Mines of Gundabad", setName, 3, 3)
+                .WithTraits("Underground.", "Dark.")
+                .WithKeywords("Deep.")
+                .WithText("Dagnir gets -10 engagement cost.")
+                .WithShadow("Shadow: If attacking enemy is Dagnir, the defending player must assign X damage among characters he controls, where X is 1 more than the number of quest cards in the victory display.")
+                .WithTemplate("<p class='main-text'>{keyword:Deep.}</p><p class='main-text'>Dagnir gets -10 engagement cost.</p>{shadow}<p class='shadow-text'><b>Shadow:</b> If attacking enemy is Dagnir, the defending player must assign X damage among characters he controls, where X is 1 more than the number of quest cards in the victory display.</p>")
+                .WithEasyModeQuantity(2)
+                .WithInfo(129, 3, Artist.Mariusz_Gandzel);
+            addLocation("Narrow Stair", setName, 2, 3)
+                .WithTraits("Underground.")
+                .WithKeywords("Deep.")
+                .WithText("When faced with the option to travel, the players must travel to Narrow Stair.")
+                .WithShadow("Shadow: Until the end of the round, defending character cannot be readied by player card effects.")
+                .WithTemplate("<p class='main-text'>{keyword:Deep.}</p><p class='main-text'>When faced with the option to travel, the players must travel to {self}.</p>{shadow}<p class='shadow-text'><b>Shadow:</b> Until the end of the round, defending character cannot be readied by player card effects.</p>")
+                .WithEasyModeQuantity(1)
+                .WithInfo(130, 2, Artist.Mariusz_Gandzel);
+            addLocation("Desecrated Ruins", setName, 2, 4)
+                .WithTraits("Underground.", "Dark.")
+                .WithKeywords("Deep.")
+                .WithText("While there are 3 or more quest cards in the victory display, Desecrated Ruins gets +2 Threat.")
+                .WithShadow("Shadow: Attacking enemy gets +1 Attack. If this attack destroys a character, each player discards a random card from his hand.")
+                .WithTemplate("<p class='main-text'>{keyword:Deep.} While there are 3 or more quest cards in the victory display, {self} gets +2 {Threat}.</p>{shadow}<p class='shadow-text'><b>Shadow:</b> Attacking enemy gets +1 {Attack}. If this attack destroys a character, each player discards a random card from his hand.</p>")
+                .WithInfo(131, 2, Artist.Mariusz_Gandzel);
+            addTreachery("Wilderland's Bane", setName)
+                .WithText("When Revealed: Dagnir makes an immediate attack against each engaged player. If no attacks were made this way, Dagnir gets -30 engagement cost until the end of the round.")
+                .WithShadow("Shadow: Attacking enemy makes an additional attack against you after this one.")
+                .WithEasyModeQuantity(0)
+                .WithInfo(132, 2, Artist.Jokubas_Uoginitas);
+            addTreachery("Suffocating Shadows", setName)
+                .WithText("When Revealed: Each player assigns X damage among allies he controls, where X is 1 more than the number of quest cards in the victory display.")
+                .WithShadow("Shadow: Attacking enemy gets +1 Attack (+3 Attack instead if there are 3 or more quests in the victory display).")
+                .WithInfo(133, 2, Artist.Jokubas_Uoginitas);
         }
     }
 }
