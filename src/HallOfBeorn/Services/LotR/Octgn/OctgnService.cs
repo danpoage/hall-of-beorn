cfb177747aa3c55d6118923b0b84bdcc54af7e8b
Dan Poage <dan.poage@gmail.com>
2020-04-19 00:40:49 -0500

    Finished changes to OctgnService

--- src/HallOfBeorn/Services/LotR/Octgn/OctgnService.cs
+++ src/HallOfBeorn/Services/LotR/Octgn/OctgnService.cs
@@ -46,6 +46,57 @@ namespace HallOfBeorn.Services.LotR.Octgn
             addSet(new Sets.AgainstTheShadow.TheDruadanForestSet());
             addSet(new Sets.AgainstTheShadow.TheMorgulValeSet());
             addSet(new Sets.AgainstTheShadow.TheStewardsFearSet());
+            addSet(new Sets.TheVoiceOfIsengard.TheVoiceOfIsengardSet());
+            addSet(new Sets.TheRingMaker.CelebrimborsSecretSet());
+            addSet(new Sets.TheRingMaker.TheAntleredCrownSet());
+            addSet(new Sets.TheRingMaker.TheDunlandTrapSet());
+            addSet(new Sets.TheRingMaker.TheNinInEilphSet());
+            addSet(new Sets.TheRingMaker.TheThreeTrialsSet());
+            addSet(new Sets.TheRingMaker.TroubleInTharbadSet());
+            addSet(new Sets.TheLostRealm.TheLostRealmSet());
+            addSet(new Sets.AngmarAwakened.AcrossTheEttenmoorsSet());
+            addSet(new Sets.AngmarAwakened.EscapeFromMountGramSet());
+            addSet(new Sets.AngmarAwakened.TheBattleOfCarnDumSet());
+            addSet(new Sets.AngmarAwakened.TheDreadRealmSet());
+            addSet(new Sets.AngmarAwakened.TheTreacheryOfRhudaurSet());
+            addSet(new Sets.AngmarAwakened.TheWastesOfEriadorSet());
+            addSet(new Sets.TheGreyHavens.TheGreyHavensSet());
+            addSet(new Sets.DreamChaser.AStormOnCobasHavenSet());
+            addSet(new Sets.DreamChaser.FlightOfTheStormcallerSet());
+            addSet(new Sets.DreamChaser.TempleOfTheDeceivedSet());
+            addSet(new Sets.DreamChaser.TheCityOfCorsairsSet());
+            addSet(new Sets.DreamChaser.TheDrownedRuinsSet());
+            addSet(new Sets.DreamChaser.TheThingInTheDepthsSet());
+            addSet(new Sets.TheSandsOfHarad.TheSandsOfHaradSet());
+            addSet(new Sets.Haradrim.BeneathTheSandsSet());
+            addSet(new Sets.Haradrim.TheBlackSerpentSet());
+            addSet(new Sets.Haradrim.TheCrossingsOfPorosSet());
+            addSet(new Sets.Haradrim.TheDungeonsOfCirithGuratSet());
+            addSet(new Sets.Haradrim.TheMumakilSet());
+            addSet(new Sets.Haradrim.TheRaceAcrossHaradSet());
+            addSet(new Sets.TheWildsOfRhovanion.TheWildsOfRhovanionSet());
+            addSet(new Sets.EredMithrin.FireInTheNightSet());
+            addSet(new Sets.EredMithrin.MountGundabadSet());
+            addSet(new Sets.EredMithrin.RoamAcrossRhovanionSet());
+            addSet(new Sets.EredMithrin.TheFateOfWilderlandSet());
+            addSet(new Sets.EredMithrin.TheGhostOfFramsburgSet());
+            addSet(new Sets.EredMithrin.TheWitheredHeathSet());
+            addSet(new Sets.AShadowInTheEast.AShadowInTheEastSet());
+            addSet(new Sets.VengeanceOfMordor.ChallengeOfTheWainridersSet());
+            addSet(new Sets.VengeanceOfMordor.TheCityOfUlfastSet());
+            addSet(new Sets.VengeanceOfMordor.WrathAndRuinSet());
+
+            addSet(new Sets.TheHobbit.TheHobbitOnTheDoorstepSet());
+            addSet(new Sets.TheHobbit.TheHobbitOverHillAndUnderHillSet());
+
+            addSet(new Sets.TheLordOfTheRings.TheBlackRidersSet());
+            addSet(new Sets.TheLordOfTheRings.TheFlameOfTheWestSet());
+            addSet(new Sets.TheLordOfTheRings.TheLandOfShadowSet());
+            addSet(new Sets.TheLordOfTheRings.TheMountainOfFireSet());
+            addSet(new Sets.TheLordOfTheRings.TheRoadDarkensSet());
+            addSet(new Sets.TheLordOfTheRings.TheTreasonOfSarumanSet());
+
+            addSet(new Sets.Starter.TwoPlayerLimitedEditionStarterSet());
         }
 
         private void addSet(OctgnSet octgnSet)
