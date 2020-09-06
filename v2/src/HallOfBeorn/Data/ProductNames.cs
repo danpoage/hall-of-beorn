39c01acb76a64c6fbf7506558e14c0505c2a8a1e
Dan Poage <daniel.poage@shipstation.com>
2020-09-06 15:35:01 -0500

    More work defining product names

--- v2/src/HallOfBeorn/Data/ProductNames.cs
+++ v2/src/HallOfBeorn/Data/ProductNames.cs
@@ -30,6 +30,57 @@ namespace HallOfBeorn.Data
         public const string TheBloodOfGondor = "The Blood of Gondor";
         public const string TheMorgulVale = "The Morgul Vale";
 
+        public const string TheVoiceOfIsengard = "The Voice of Isengard";
+        public const string TheDunlandTrap = "The Dunland Trap";
+        public const string TheThreeTials = "The Three Trials";
+        public const string TroubleInTharbad = "Trouble in Tharbad";
+        public const string TheNinInEilph = "The Nîn-in-Eilph";
+        public const string CelebrimborsSecret = "Celebrimbor's Secret";
+        public const string TheAntleredCrown = "The Antlered Crown";
+
+        public const string TheLostRealm = "The Lost Realm";
+        public const string TheWastesOfEriador = "The Wastes of Eriador";
+        public const string EscapeFromMountGram = "Escape from Mount Gram";
+        public const string AcrossTheEttenmoors = "Across the Ettenmoors";
+        public const string TheTreacheryOfRhudaur = "The Treachery of Rhudaur";
+        public const string TheBattleOfCarnDum = "The Battle of Carn Dûm";
+        public const string TheDreadRealm = "The Dread Realm";
+
+        public const string TheGreyHavens = "The Grey Havens";
+        public const string FlightOfTheStormcaller = "Flight of the Stormcaller";
+        public const string TheThingInTheDepths = "The Thing in the Depths";
+        public const string TempleOfTheDeceived = "Temple of the Deceived";
+        public const string TheDrownedRuines = "The Drowned Ruins";
+        public const string AStormOnCobasHaven = "A Storm on Cobas Haven";
+        public const string TheCityOfCorsairs = "The City of Corsairs";
+
+        public const string TheSandsOfHarad = "The Sands of Harad";
+        public const string TheMumakil = "";
+        public const string RaceAcrossHarad = "Race Across Harad";
+        public const string BeneathTheSands = "Beneath the Sands";
+        public const string TheBlackSerpent = "The Black Serpent";
+        public const string TheDungeonsOfCirithGurat = "The Dungeons of Cirith Gurat";
+        public const string TheCrossingsOfPoros = "The Crossings of Poros";
+
+        public const string TheWildsOfRhovanion = "The Wilds of Rhovanion";
+        public const string TheWitheredHeath = "The Withered Heath";
+        public const string RoamAcrossRhovanion = "Roam Across Rhovanion";
+        public const string FireInTheNight = "Fire in the Night";
+        public const string TheGhostOfFramsburg = "The Ghost of Framsburg";
+        public const string MountGundabad = "Mount Gundabad";
+        public const string TheFateOfWilderland = "The Fate of Wilderland";
+
+        public const string AShadowInTheEast = "A Shadow in the East";
+        public const string WrathAndRuin = "Wrath and Ruin";
+        public const string TheCityOfUlfast = "The City of Ulfast";
+        public const string ChallengeOfTheWainriders = "Challenge of the Wainriders";
+        public const string UnderTheAshMountains = "Under the Ash Mountains";
+        public const string TheLandOfSorrow = "The Land of Sorrow";
+        public const string TheFortressOfNurn = "The Fortress of Nurn";
+
+        public const string TheHobbitOverHillAndUnderHill = "The Hobbit: Over Hill and Under Hill";
+        public const string TheHobbitOnTheDoorstep = "The Hobbit: On the Doorstep";
+
         public const string TheBlackRiders = "The Black Riders";
         public const string TheRoadDarkens = "The Road Darkens";
     }
