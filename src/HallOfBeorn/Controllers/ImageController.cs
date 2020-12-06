b9f4ec107a02157dae33aabdfbd80e2d34e4693b
Dan Poage <dan.poage@gmail.com>
2020-12-05 22:57:19 -0600

    Added CardFront endpoint to the Image controller

--- src/HallOfBeorn/Controllers/ImageController.cs
+++ src/HallOfBeorn/Controllers/ImageController.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using HallOfBeorn.Models.LotR.ViewModels;
+using HallOfBeorn.Services.LotR;
+
 namespace HallOfBeorn.Controllers
 {
     public class ImageController : Controller
@@ -18,6 +21,21 @@ namespace HallOfBeorn.Controllers
             return Redirect(string.Format("https://s3.amazonaws.com/hallofbeorn-resources/Images/Cards/{0}/{1}", setName, imageName));
         }
 
+        public ActionResult CardFront(string id)
+        {
+            var productRepo = new ProductRepository();
+            var cardRepo = new LotRCardRepository(productRepo);
+
+            var card = cardRepo.FindBySlug(id);
+
+            if (card == null)
+            {
+                return HttpNotFound("Card Image Not Found");
+            }
+
+            var viewModel = new CardViewModel(card, 0, null, null, null, null, null);
+            return Redirect(viewModel.ImagePath);
+        }
 
         public ActionResult GetProductImage(string imageName)
         {
