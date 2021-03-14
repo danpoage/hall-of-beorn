1a031f3384f2755a45c15737375b56d872c4cf35
Dan Poage <dan.poage@gmail.com>
2021-03-14 01:51:24 -0600

    Created CardPredicate helper class. Started defining archetypes: Ent Retaliation, Gondor Swarm, Gondor Traps, and Gondor Valour.

--- src/HallOfBeorn/Services/LotR/Categories/ArchetypeService.cs
+++ src/HallOfBeorn/Services/LotR/Categories/ArchetypeService.cs
@@ -13,10 +13,10 @@ namespace HallOfBeorn.Services.LotR.Categories
         {
         }
 
-        private Func<LotRCard, Archetype> CreateArchetypeFilter(Func<LotRCard, bool> predicate, Archetype archetype)
+        private Func<LotRCard, Archetype> CreateArchetypeFilter(CardPredicate<Archetype> predicate)
         {
             return new Func<LotRCard, Archetype>((card) => {
-                return (predicate(card)) ? archetype : Archetype.None;
+                return predicate.Eval(card) ? predicate.Category : Archetype.None;
             });
         }
 
@@ -27,12 +27,46 @@ namespace HallOfBeorn.Services.LotR.Categories
 
         protected override List<Func<LotRCard, Archetype>> GetCategoryFilters()
         {
-            Func<LotRCard, bool> isGondorSwarm = c => c.NormalizedTitle.ContainsAny("guard of the citadel", "squire of the citadel", "defender of rammas",
-                "steward of gondor", "rod of the steward", "for gondor", "visionary leadership",
-                    "denethor", "boromir");
+            var gondorSwarm = new CardPredicate<Archetype>(Archetype.Gondor_Swarm)
+                .Hero("Boromir", Sphere.Leadership)
+                .Hero("Denethor", Sphere.Leadership)
+                .Hero("Prince Imrahil")
+                .Hero("Lothiriel")
+                .Hero("Hirgon")
+                .Ally("Faramir", Sphere.Leadership)
+                .Titles("Steward of Gondor", "Visionary Leadership", "For Gondor")
+                .Or(c => c.CardType == CardType.Ally
+                    && c.NormalizedTraits.Any(t => t == "Gondor.")
+                    && !c.NormalizedTraits.Any(t => t == "Ranger.")
+                    && c.ResourceCost <= 2);
+
+            var gondorTraps = new CardPredicate<Archetype>(Archetype.Gondor_Traps)
+                .Hero("Damrod", Sphere.Lore)
+                .Hero("Mablung", Sphere.Tactics)
+                .Hero("Denethor", Sphere.Lore)
+                .Hero("Faramir", Sphere.Lore)
+                .Ally("Faramir", Sphere.Lore)
+                .Titles("Ranger Spear", "Ranger Bow", "Arrows from the Trees", "Take No Notice")
+                .Trait("Trap")
+                .AllTraits("Gondor.", "Ranger.")
+                .Or(c => c.Text.ContainsLower("attached trap"));
+
+            var gondorValour = new CardPredicate<Archetype>(Archetype.Gondor_Valour)
+                .Hero("Boromir", Sphere.Tactics)
+                .Hero("Hirgon")
+                .AnyText("Valour", "40")
+                .Or(c => c.NormalizedTraits.Any(t => t == "Gondor.") && c.Text.ContainsLower("doomed "))
+                .Not(c => c.Title == "Knight of Dale");
+
+            var entRetaliation = new CardPredicate<Archetype>(Archetype.Ent_Retaliation)
+                .Trait("Ent.")
+                .AllText(" Ent ", "Damage");
 
             return new List<Func<LotRCard, Archetype>> {
-                CreateArchetypeFilter(isGondorSwarm, Archetype.Gondor_Swarm),
+                CreateArchetypeFilter(entRetaliation),
+                CreateArchetypeFilter(gondorSwarm),
+                CreateArchetypeFilter(gondorTraps),
+                CreateArchetypeFilter(gondorValour),
             };
         }
     }
---
+++ src/HallOfBeorn/Services/LotR/Categories/CardPredicate.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using HallOfBeorn.Models.LotR;
+
+namespace HallOfBeorn.Services.LotR.Categories
+{
+    public class CardPredicate<T>
+    {
+        public CardPredicate(T category)
+        {
+            this.category = category;
+        }
+
+        private readonly T category;
+        private readonly List<Func<LotRCard, bool>> predicates = new List<Func<LotRCard, bool>>();
+        private readonly List<Func<LotRCard, bool>> exclusions = new List<Func<LotRCard, bool>>();
+
+        public T Category { get { return category; } }
+
+        public CardPredicate<T> Or(Func<LotRCard,bool> predicate)
+        {
+            predicates.Add(predicate);
+            return this;
+        }
+
+        public CardPredicate<T> Not(Func<LotRCard, bool> predicate)
+        {
+            exclusions.Add(predicate);
+            return this;
+        }
+
+        public CardPredicate<T> Titles(params string[] titles)
+        {
+            return Or(card => titles.Any(title => card.NormalizedTitle.ContainsLower(title)));
+        }
+
+        public CardPredicate<T> Hero(string title)
+        {
+            return Or(card => card.CardType == CardType.Hero
+                && card.NormalizedTitle.ContainsLower(title));
+        }
+
+        public CardPredicate<T> Hero(string title, Sphere sphere)
+        {
+            return Or(card => card.CardType == CardType.Hero
+                && card.NormalizedTitle.ContainsLower(title) && card.Sphere == sphere);
+        }
+
+        public CardPredicate<T> Ally(string title)
+        {
+            return Or(card => card.CardType == CardType.Ally
+                && card.NormalizedTitle.ContainsLower(title));
+        }
+
+        public CardPredicate<T> Ally(string title, Sphere sphere)
+        {
+            return Or(card => card.CardType == CardType.Ally
+                && card.NormalizedTitle.ContainsLower(title) && card.Sphere == sphere);
+        }
+
+        public CardPredicate<T> Trait(string trait)
+        {
+            return Or(card => 
+                card.NormalizedTraits.Any(cardTrait => cardTrait == trait));
+        }
+
+        public CardPredicate<T> AllTraits(params string[] traits)
+        {
+            return Or(card => 
+                traits.All(t => card.NormalizedTraits.Any(nt => nt == t)));
+        }
+
+        public CardPredicate<T> AllText(params string[] tokens)
+        {
+            return Or(card =>
+                tokens.All(token => card.Text.ContainsLower(token)));
+        }
+
+        public CardPredicate<T> AnyText(params string[] tokens)
+        {
+            return Or(card =>
+                tokens.Any(token => card.Text.ContainsLower(token)));
+        }
+
+
+
+        public bool Eval(LotRCard card)
+        {
+            if (exclusions.Any(ex => ex(card)))
+            {
+                return false;
+            }
+
+            return predicates.Any(f => f(card));
+        }
+    }
+}
