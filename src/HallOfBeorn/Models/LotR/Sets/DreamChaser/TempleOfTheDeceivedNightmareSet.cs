04c75ffe4e1f593c444726c86f0411b15b33ee2a
Dan Poage <dan.poage@gmail.com>
2021-04-08 22:15:15 -0500

    Fixed counts for Temple of the Deceived Nightmare cards.

--- src/HallOfBeorn/Models/LotR/Sets/DreamChaser/TempleOfTheDeceivedNightmareSet.cs
+++ src/HallOfBeorn/Models/LotR/Sets/DreamChaser/TempleOfTheDeceivedNightmareSet.cs
@@ -56,21 +56,21 @@ Finally, flip this setup card over and place it next to the quest deck. Its effe
                 .WithText("Forced: When Desecrated Ruins is flipped over while active, remove all resources from each hero and objective-ally.")
                 .WithFlavor("Tar-Palantir attempted to ammend the evil: but it was too late, and there was rebellion and strife in Númenor\r\n-The Return of the King")
                 .WithOppositeText("Uncharted. Cannot have player card attachments.\r\nForced: After Edge of the Temple becomes the active location, flip it over.\r\nAction: Remove 8 progress from Edge of the Temple to look at its facedown side.")
-                .WithInfo(2, 3, Artist.Federico_Musetti);
+                .WithInfo(2, 2, Artist.Federico_Musetti);
             addLocation("Temple Foothills", EncounterSet.TempleOfTheDeceivedNightmare.Name, 2, 5)
                 .WithOppositeTitle("Edge of the Temple")
                 .WithTraits("Ruins.")
                 .WithKeywords("Uncharted.")
                 .WithText("While Temple Foothills is the active location, the players, as a group, cannot play more than 1 card each round.")
                 .WithOppositeText("Uncharted. Cannot have player card attachments.\r\nForced: After Edge of the Temple becomes the active location, flip it over.\r\nAction: Remove 8 progress from Edge of the Temple to look at its facedown side.")
-                .WithInfo(3, 3, Artist.Julian_Kok);
+                .WithInfo(3, 2, Artist.Julian_Kok);
             addLocation("Cursed Temple", EncounterSet.TempleOfTheDeceivedNightmare.Name, 3, 4)
                 .WithOppositeTitle("Edge of the Temple")
                 .WithTraits("Ruins.")
                 .WithKeywords("Uncharted.")
                 .WithText("Forced: When Cursed Temple is flipped over while active, discard 3 cards at random from each player's hand.\r\nWhile Cursed Temple is the active location, each player cannot draw cards by card effects.")
                 .WithOppositeText("Uncharted. Cannot have player card attachments.\r\nForced: After Edge of the Temple becomes the active location, flip it over.\r\nAction: Remove 8 progress from Edge of the Temple to look at its facedown side.")
-                .WithInfo(4, 3, Artist.Julian_Kok);
+                .WithInfo(4, 2, Artist.Julian_Kok);
             addEnemy("King's Knight", EncounterSet.TempleOfTheDeceivedNightmare.Name, 23, 4, 4, 4, 6)
                 .WithTraits("Undead.")
                 .WithText("Forced: After King's Knight attacks, if no damage was dealt by its attack, return it to the staging area.")
@@ -79,7 +79,7 @@ Finally, flip this setup card over and place it next to the quest deck. Its effe
             addEnemy("Cursed Soldier", EncounterSet.TempleOfTheDeceivedNightmare.Name, 28, 3, 3, 3, 4)
                 .WithTraits("Undead.")
                 .WithText("While the active location is in the top rown, Cursed Soldier gets +3 Threat.\r\nWhile the active location is in the middle row, Cursed Soldier gets +3 Attack.\r\nWhile the active location is in the bottom row, Cursed Soldier gets +3 Defense.")
-                .WithInfo(6, 2, Artist.Chris_Grun);
+                .WithInfo(6, 3, Artist.Chris_Grun);
             addTreachery("Deceitful Island", EncounterSet.TempleOfTheDeceivedNightmare.Name)
                 .WithKeywords("Doomed 3.")
                 .WithTextLine("Doomed 3.")
@@ -88,11 +88,11 @@ Finally, flip this setup card over and place it next to the quest deck. Its effe
             addTreachery("Call of the Curse", EncounterSet.TempleOfTheDeceivedNightmare.Name)
                 .WithText("When Revealed: Until the end of the round, each Undead enemy gets +X Attack and +X Defense. X is the number of locations to the left of the active location (in its row). If X is 0 or 1, Call of the Curse gains surge.")
                 .WithShadow("Shadow: Resolve the 'when revealed' effect of Call of the Curse.")
-                .WithInfo(8, 2, Artist.Monztre);
+                .WithInfo(8, 3, Artist.Monztre);
             addTreachery("Sudden Precipice", EncounterSet.TempleOfTheDeceivedNightmare.Name)
                 .WithText("When Revealed: Attach to the active location. (Counts as a Condition attachment with the text: \"Forced: After you travel from attached location to the right, deal 2 damage to each exhausted character.\")")
                 .WithShadow("Shadow: Attacking enemy makes an additional attack after this one.")
-                .WithInfo(8, 3, Artist.Federico_Musetti);
+                .WithInfo(9, 3, Artist.Federico_Musetti);
         }
     }
 }
