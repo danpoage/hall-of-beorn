07d21073e4eb7717ff5bbef4a3b08a0adf8d3e28
Dan Poage <dan.poage@gmail.com>
2021-04-20 20:54:42 -0500

    Added autocomplete for First Age and ALeP cards, when those sets are enabled in settings.

---
+++ src/HallOfBeorn/Scripts/cookie-1.0.1.js
@@ -0,0 +1,19 @@
+﻿function getCookie(cname) {
+
+    if (!document.cookie) {
+        return "";
+    }
+
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+};
