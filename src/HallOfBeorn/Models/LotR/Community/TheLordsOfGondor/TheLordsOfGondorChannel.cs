c1d74bd954d12e584db6097b7bf880586bfdd561
Dan Poage <daniel.poage@shipstation.com>
2021-07-16 15:42:08 -0500

    Added Vision of the Palantir and The Lords of Gondor links

---
+++ src/HallOfBeorn/Models/LotR/Community/TheLordsOfGondor/TheLordsOfGondorChannel.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace HallOfBeorn.Models.LotR.Community.LordsOfGondor
+{
+    public class TheGreyCompanyChannel
+        : Creator
+    {
+        public LordsOfChannel()
+            : base("Lords of Gondor Channel",
+            "https://www.youtube.com/channel/UC09txNMi_RVwmZRGLzdpYUg",
+            "https://www.youtube.com/feeds/videos.xml?channel_id=UC09txNMi_RVwmZRGLzdpYUg",
+            LinkType.Lords_of_Gondor)
+        {
+            AddYear(2021);
+            AddLink("Nightmare Flight from Moria 2P - Lords of Gondor S2 Ep 3", 
+                "https://www.youtube.com/watch?v=X_kEWtfBFBk", "Jul 14 2021");
+            AddLink("Nightmare The Seventh Level 2P - Lords of Gondor S2 Ep 2 - Lotr LCG - One Fellowship Run", 
+                "https://www.youtube.com/watch?v=6Cp4C4U1Zsc", "Jul 13 2021");
+            AddLink("Nightmare Dungeons Deep and Caverns Dim Solo - The Best Deck Season H Ep 3 - Lotr LCG - One Deck Run", 
+                "https://www.youtube.com/watch?v=Be3P-c7flDw", "Jul 13 2021");
+            AddLink("Nightmare Over The Misty Mountains Grim Solo - The Best Deck Season H Ep 2 - Lotr LCH - One Deck Run", 
+                "https://www.youtube.com/watch?v=lSNqMlnp8tQ", "Jul 12 2021");
+            AddLink("Nightmare We Must Away, Ere Break of Day Solo - The Best Deck Season H Premiere - One Deck Run", 
+                "https://www.youtube.com/watch?v=RXavbf8SvwU", "Jul 6 2021");
+            AddLink("Nightmare Into the Pit 2P - Lords of Gondor S2 Premiere - Same Decks Every Quest", 
+                "https://www.youtube.com/watch?v=qOEujzbgLIE", "Jul 5 2021");
+            AddLink("Nightmare Return to Mirkwood Solo - The Best Deck S1 Finale - Same Deck Every Quest", 
+                "https://www.youtube.com/watch?v=DS8gZCv1Qcw", "Jun 27 2021");
+            AddLink("Attack on Dol Guldur 2P - Lords of Gondor S1 Finale - Lotr LCG - Same Decks Every Quest", 
+                "https://www.youtube.com/watch?v=x0ZyohxH9xM", "Jun 25 2021");
+            AddLink("Nightmare Return to Mirkwood 2P - Lords of Gondor S1 Ep 9 - Same Decks Every Quest", 
+                "https://www.youtube.com/watch?v=yJ7yxZXCdYM", "Jun 24 2021");
+            AddLink("Nightmare The Dead Marshes 2P - The Lords of Gondor S1 Ep 8 - Same Deck Every Quest", 
+                "https://www.youtube.com/watch?v=3BMdw6TiwrQ", "Jun 16 2021");
+            AddLink("Nightmare The Dead Marshes Solo - The Best Deck S1 Ep 8 - Same Deck Every Quest", 
+                "https://www.youtube.com/watch?v=zJ5anliHCQc", "Jun 16 2021");
+            AddLink("Nightmare The Hills of Emyn Muil Solo - The Best Deck S1 Ep 7 - Lotr LCG - Same Deck Every Quest", 
+                "https://www.youtube.com/watch?v=MtLkCDV5Oag", "Jun 12 2021");
+            AddLink("Nightmare A Journey to Rhosgobel Solo - The Best Deck S1 Ep 6", 
+                "https://www.youtube.com/watch?v=wjK8-KPl6PY", "Jun 9 2021");
+            AddLink("The Grima Issue - The Best Season 1 Intermission", 
+                "https://www.youtube.com/watch?v=FRealo_vXlg", "Jun 9 2021");
+            AddLink("Nightmare The Hills of Emyn Muil 2P - Lords of Gondor S1 Ep 7 - Lotr LCG - Same Decks Every Quest", 
+                "https://www.youtube.com/watch?v=HINEOQ81iGU", "Jun 5 2021");
+            AddLink("Nightmare Conflict at the Carrock Solo - Lotr LCG - The Best Deck S1 Ep 5 - Same Deck Every Quest", 
+                "https://www.youtube.com/watch?v=hoyWgMwS0Z4", "Jun 3 2021");
+            AddLink("Nightmare The Hunt for Gollum Solo - The Best Deck S1 Ep 4 - Lotr LCG - The Same Deck Every Quest", 
+                "https://www.youtube.com/watch?v=VOnSAfb6Ta0", "Jun 1 2021");
+            AddLink("Nightmare Journey to Rhosgobel 2P - The Lords of Gondor S1 Ep 6 - Lotr LCG - One Fellowship Run", 
+                "https://www.youtube.com/watch?v=UHFoNSIM7e0", "May 31 2021");
+            AddLink("Nightmare Escape from Dol Guldur Solo - The Best Deck S1 Ep 3 - Lotr LCG - Same Deck Every Quest", 
+                "https://www.youtube.com/watch?v=pkQauNbCWto", "May 28 2021");
+            AddLink("Nightmare Journey Along the Anduin Solo - The Best Deck S1 Ep 2 - Lotr LCG - Same Every Quest", 
+                "https://www.youtube.com/watch?v=GMUKG018_Rk", "May 28 2021");
+            AddLink("Nightmare Passage Through Mirkwood Solo - The Best Deck S1 Premiere - Same Deck Every Quest", 
+                "https://www.youtube.com/watch?v=FF1Hpy0nsCU", "May 28 2021");
+            AddLink("The History of One Decks and Introduction to the Best Deck - Prologue Episode", 
+                "https://www.youtube.com/watch?v=CLH1JCwhL1o", "May 28 2021");
+            AddLink("Nightmare Conflict as the Carrock 2P - The Lords of Gondor S1 Ep 5 - Lotr LCG - One Fellowship Run", 
+                "https://www.youtube.com/watch?v=BKGtem9yhII", "May 26 2021");
+            AddLink("Nightmare The Hunt for Gollum 2P - Lords of Gondor S1 Ep 4 - Lotr LCG - Same Two Decks Every Quest", 
+                "https://www.youtube.com/watch?v=J43ligCIHWk", "May 19 2021");
+            AddLink("Nightmare Escape from Dol Guldur 2P - The Lords of Gondor S1 Ep 3 - Lotr LCG - One Fellowship Run", 
+                "https://www.youtube.com/watch?v=pMZkMJElWgo", "May 17 2021");
+            AddLink("Nightmare Journey Along the Anduin 2P - The Lords of Gondor S1 Ep 2 - Lotr LCG - One Fellowship Run", 
+                "https://www.youtube.com/watch?v=9ZaS8rdYWSM", "May 16 2021");
+            AddLink("Nightmare Passage Through Mirkwood 2P - The Lords of Gondor S1 Premier - Lotr LCG - One Fellowship",
+                "https://www.youtube.com/watch?v=xMwkojqmsJg", "May 16 2021");
+        }
+    }
+}
